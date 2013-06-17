feadae8be27cecf7915270c48de71e485748aadf
marcus <marcus@9d5b8971-822b-0410-80eb-d18c1038ef23>
2013-06-17 15:59:36 +0000

    Add class to load modules and scripts
    git-svn-id: http://svn.cc.gatech.edu/fastlab/mlpack/conf/jenkins-conf/benchmark@15243 9d5b8971-822b-0410-80eb-d18c1038ef23

---
+++ util/loader.py
@@ -0,0 +1,49 @@
+'''
+  @file loader.py
+  @author Marcus Edel
+
+  Class to load modules and scripts.
+'''
+
+import imp
+import os
+
+
+class Loader(object):
+
+  # Import a module from a path.
+  @staticmethod
+  def ImportModuleFromPath(path):
+
+    if hasattr(os, 'getcwdu'):
+      # Returns a Unicode object represantation.
+      realPath = os.path.realpath(os.getcwdu())
+    else:
+      realPath = os.path.realpath(os.path.curdir)
+      
+    destinationPath = os.path.dirname(path)
+
+    if destinationPath == '':
+      destinationPath = '.'
+      
+    # Remove the .py suffix.
+    scriptName = os.path.basename(path)
+
+    if scriptName.endswith('.py'):
+      modName = scriptName[:-3]
+    else:
+      modName = scriptName
+      
+    os.chdir(destinationPath)
+    fileHandle = None
+    try:
+      tup = imp.find_module(modName, ['.'])
+      module = imp.load_module(modName, *tup)
+      fileHandle = tup[0]
+    finally:
+      os.chdir(realPath)
+      if fileHandle is not None:
+        fileHandle.close()
+
+    # Return module name.
+    return module
