1d8bc24f8122d4cc3f04fa762a79f31ab7334257
marcus <marcus@9d5b8971-822b-0410-80eb-d18c1038ef23>
2013-07-03 14:22:09 +0000

    Add timer class to measure time in python.
    git-svn-id: http://svn.cc.gatech.edu/fastlab/mlpack/conf/jenkins-conf/benchmark@15388 9d5b8971-822b-0410-80eb-d18c1038ef23

---
+++ util/timer.py
@@ -0,0 +1,31 @@
+'''
+  @file log.py
+  @author Marcus Edel
+
+  Implementation of the timer class.
+'''
+
+from __future__ import with_statement
+import time
+
+'''
+This class implements three functions to measure the time.
+'''
+class Timer(object):
+  '''
+  Start the timer.
+  '''
+  def __enter__(self):
+    self.__start = time.time()
+
+  '''
+  Stop the timer.
+  '''
+  def __exit__(self, type, value, traceback):
+    self.__finish = time.time()
+  
+  '''
+  Return the elapsed time of the timer.
+  '''
+  def ElapsedTime(self):
+    return self.__finish - self.__start
