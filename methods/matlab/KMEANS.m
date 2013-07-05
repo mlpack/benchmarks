704f4ebb09d58f5541d9e0e65de7f2c9dd296659
marcus <marcus@9d5b8971-822b-0410-80eb-d18c1038ef23>
2013-07-05 16:57:20 +0000

    Add matlab K-Means method and K-Means benchmark script.
    git-svn-id: http://svn.cc.gatech.edu/fastlab/mlpack/conf/jenkins-conf/benchmark@15418 9d5b8971-822b-0410-80eb-d18c1038ef23

---
+++ methods/matlab/KMEANS.m
@@ -0,0 +1,61 @@
+% @file KMEANS.m
+% @author Marcus Edel
+%
+% K-Means Clustering with matlab.
+
+function KMEANS(cmd)
+% This program performs K-Means clustering on the given dataset
+%
+% Required options:
+%     (-i) [string]    Input dataset to perform clustering on.
+% Options:
+%  (-c) [int]          Number of clusters to find.
+%  (-m) [int]          Maximum number of iterations before K-Means 
+%                      terminates. Default value 1000.
+%  (-s) [int]          Random seed. If 0, 'std::time(NULL)' is used.
+
+
+% Load input dataset.
+inputFile = regexp(cmd, '.*?-i ([^\s]+)', 'tokens', 'once');
+X = csvread(inputFile{:});
+
+% Check if centroid starting locations set is given.
+C = [];
+if strfind(cmd, '-I') > 0
+    centroidFile = regexp(cmd, '.*?-I ([^\s]+)', 'tokens', 'once');
+    C = csvread(centroidFile{:});
+end
+
+% Gather parameters.
+clusters = str2double(regexp(cmd,'.* -c (\d+)','tokens','once'));
+maxIterations = str2double(regexp(cmd,'.* -m (\d+)','tokens','once'));
+seed = str2double(regexp(cmd,'.* -s (\d+)','tokens','once'));
+
+% Validate parameters.
+if isempty(maxIterations)
+  m = 1000;
+else
+  if maxIterations == 0
+    m = inf;
+  elseif maxIterations
+    m = maxIterations;
+  end
+end
+
+if ~isempty(seed)
+  s = RandStream('mt19937ar','Seed', seed);
+  RandStream.setGlobalStream(s);
+end
+
+total_time = tic;
+if ~isempty(clusters)
+    [IDX, C] = kmeans(X, clusters, 'EmptyAction', 'singleton', ...
+            'MaxIter', m);
+    disp(sprintf('[INFO ]   total_time: %fs', toc(total_time)))
+elseif ~isempty(C)
+    [IDX, C] = kmeans(X, size(C, 1), 'Start', C, 'EmptyAction', ...
+            'singleton', 'MaxIter', m);
+    disp(sprintf('[INFO ]   total_time: %fs', toc(total_time)))
+end
+
+end
---
+++ methods/matlab/kmeans.py
@@ -0,0 +1,121 @@
+'''
+  @file kmeans.py
+  @author Marcus Edel
+
+  Class to benchmark the matlab K-Means Clustering method.
+'''
+
+import os
+import sys
+import inspect
+
+# Import the util path, this method even works if the path contains symlinks to
+# modules.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
+	os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../util")))
+if cmd_subfolder not in sys.path:
+	sys.path.insert(0, cmd_subfolder)
+
+from log import *
+from profiler import *
+
+import shlex
+import subprocess
+import re
+import collections
+
+'''
+This class implements the K-Means Clustering benchmark.
+'''
+class KMEANS(object):
+
+	''' 
+	Create the K-Means Clustering benchmark instance.
+  
+  @param dataset - Input dataset to perform K-Means on.
+  @param path - Path to the mlpack executable.
+  @param verbose - Display informational messages.
+	'''
+	def __init__(self, dataset, path=os.environ["MATLAB_BIN"], verbose = True): 
+		self.verbose = verbose
+		self.dataset = dataset
+		self.path = path
+
+	'''
+	Destructor to clean up at the end.
+	'''
+	def __del__(self):		
+		pass	
+		
+	'''
+  Non-negative Matrix Factorization. If the method has been successfully 
+  completed return the elapsed time in seconds.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or -1 if the method was not successful.
+  '''
+	def RunMethod(self, options):
+		Log.Info("Perform K-Means.", self.verbose)
+
+		# If the dataset contains two files then the second file is the centroids 
+		# file. In this case we add this to the command line.
+		if len(self.dataset) == 2:
+			inputCmd = "-i " + self.dataset[0] + " -I " + self.dataset[1] + " " + options
+		else:
+			inputCmd = "-i " + self.dataset + " " + options
+		
+		# Split the command using shell-like syntax.
+		cmd = shlex.split(self.path + "matlab -nodisplay -nosplash -r \"try, " +
+				"KMEANS('"  + inputCmd + "'), catch, exit(1), end, exit(0)\"")
+		
+		# Run command with the nessecary arguments and return its output as a byte
+		# string. We have untrusted input so we disables all shell based features.
+		try:
+			s = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=False)		
+		except Exception:
+			Log.Fatal("Could not execute command: " + str(cmd))
+			return -1
+
+		# Return the elapsed time.
+		timer = self.parseTimer(s)
+		if not timer:
+			Log.Fatal("Can't parse the timer")
+			return -1
+		else:
+			time = self.GetTime(timer)
+			Log.Info(("total time: %fs" % time), self.verbose)
+
+			return time
+
+	'''
+	Parse the timer data form a given string.
+
+	@param data - String to parse timer data from.
+	@return - Namedtuple that contains the timer data.
+	'''
+	def parseTimer(self, data):
+		# Compile the regular expression pattern into a regular expression object to
+		# parse the timer data.
+		pattern = re.compile(r"""
+				.*?total_time: (?P<total_time>.*?)s.*?
+				""", re.VERBOSE|re.MULTILINE|re.DOTALL)
+		
+		match = pattern.match(data)
+		if not match:
+			Log.Fatal("Can't parse the data: wrong format")
+			return -1
+		else:
+			# Create a namedtuple and return the timer data.
+			timer = collections.namedtuple("timer", ["total_time"])
+			
+			return timer(float(match.group("total_time")))
+
+	'''
+	Return the elapsed time in seconds.
+
+	@param timer - Namedtuple that contains the timer data.
+	@return Elapsed time in seconds.
+	'''
+	def GetTime(self, timer):
+		time = timer.total_time
+		return time
