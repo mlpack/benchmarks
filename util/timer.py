1f53db55f4fd4d0c990187005c2e40cb6fff5e13
marcus <marcus@9d5b8971-822b-0410-80eb-d18c1038ef23>
2013-07-26 13:03:21 +0000

    Add class to handle timeouts.
    git-svn-id: http://svn.cc.gatech.edu/fastlab/mlpack/conf/jenkins-conf/benchmark@15556 9d5b8971-822b-0410-80eb-d18c1038ef23

--- util/timer.py
+++ util/timer.py
@@ -7,6 +7,10 @@
 
 from __future__ import with_statement
 import time
+from functools import wraps
+import errno
+import os
+import signal
 
 '''
 This class implements three functions to measure the time.
@@ -29,3 +33,34 @@ class Timer(object):
   '''
   def ElapsedTime(self):
     return self.__finish - self.__start
+
+'''
+This Class provides a Timout Error.
+'''
+class TimeoutError(Exception):
+    errno = 23
+
+'''
+This function implements a timeout for a function call.
+
+@param seconds - The time until the timeout. Default 5000 seconds.
+@param errorMessage - Message for the Error when the timeout is invoked.
+@return Timeout error.
+'''
+def timeout(seconds=5000, errorMessage=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def handleTimeout(signum, frame):
+            raise TimeoutError(errorMessage)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, handleTimeout)
+            signal.setitimer(signal.ITIMER_REAL,seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
