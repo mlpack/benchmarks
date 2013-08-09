d79a0b6f6af1f54152d30777c1873085c2213306
marcus <marcus@9d5b8971-822b-0410-80eb-d18c1038ef23>
2013-08-09 16:50:36 +0000

    Add css file for the report page and the new function to create the charts.
    git-svn-id: http://svn.cc.gatech.edu/fastlab/mlpack/conf/jenkins-conf/benchmark@15602 9d5b8971-822b-0410-80eb-d18c1038ef23

--- util/graph.py
+++ util/graph.py
@@ -18,7 +18,11 @@ from misc import *
 
 import numpy as np
 import matplotlib
+
+matplotlib.use("Agg")
+
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
 
 # Use this colors to plot the graph.
@@ -28,129 +32,207 @@ colors = ['#3366CC', '#DC3912', '#FF9900', '#FFFF32', '#109618', '#990099',
 '''
 Generate a bar chart with the specified informations.
 
-Data structure: list( list ) = [ [] ]
-
-@param header - Contains the names for the x-axis.
-@param data - Contains the values for the chart.
-@param chartName - Contains the name for the chart.
+@param results - Contains the values to plot.
+@param libraries - A list that contains the names of the libraries.
+@param fileName - The filename of the line chart.
+@param bestlib - The name of the library which should be compared with the other
+                 libraries.
 '''
-def GenerateBarChart(header, data, chartName):
+def GenerateBarChart(results, libraries, fileName, bestlib="mlpack"):
   # Bar chart settings.
-  barWidth = 0.25
-  opacity = 0.8
+  lineWidth = 0.1
+  barWidth = 0.15
+  opacity = 0.9
   fill = True
-  windowWidth = 12
-  windowHeight = 8
+  windowWidth = 11
+  windowHeight = 1.5
   backgroundColor = '#F5F5F5'
+  gridLineWidth = 0.2
 
   # Create figure and set the color.
   matplotlib.rc('axes', facecolor=backgroundColor)
   fig = plt.figure(figsize=(windowWidth, windowHeight), 
-      facecolor=backgroundColor)
+      facecolor=backgroundColor, dpi=80)
+  plt.rc('lines', linewidth=lineWidth)
   ax = plt.subplot(1,1,1)
 
-  # Bar and legend position.
-  barIndex = np.arange(len(header)) * 2
-  legendIndex = barIndex  
+  # Set the grid style.
+  ax.yaxis.grid(True, linestyle='-', linewidth=gridLineWidth)
+  ax.xaxis.grid(False)
+  ax.spines['left'].set_visible(False)
+  ax.spines['top'].set_visible(False)
+  ax.spines['right'].set_visible(False)
+  ax.get_xaxis().tick_bottom()
+  ax.get_yaxis().tick_left()
+  ax.spines['bottom'].set_linewidth(gridLineWidth)
 
-  for i, values in enumerate(data):
-    dataset = values[0] # Dataset name.
+  # Data structures to set the legend and the right postions for the bar chart.
+  legendIndex = []
+  color = {}
+  chartHandler = []
+  legendNames = []
+  nextBar = 0
+  legendPosition = 0
+  legendBegin = 0
+  
+  # use this variable to count the time.
+  totalTime = 0
+  # Use this variable to count the timeouts.
+  timeouts = 0
+  # Use this variable to count the failures.
+  failure = 0
 
-    # Convert values to float.
-    values = values[1:]
-    values = [float(x) if isFloat(x) else 0 for x in values]
+  # Use this data structures to generate the timing table and the progress bar.
+  bestTiming = {}
+  timingData = {}
 
-    # Get the bar chart position for the dataset.
-    barIndex = [barIndex[j] + barWidth if x != 0 and i != 0 else barIndex[j] 
-        for j, x in enumerate(values)]
+  # Use this variable to get use the data for the right library.
+  l = 0 
 
-    # Plot the bar chart with the defined setting.
-    foo = plt.bar(barIndex, values, barWidth, alpha=opacity, 
-        color=colors[i%len(colors)], label=dataset, fill=fill, lw=0.2)
-
-    # Get the position for the x-axis legend.
-    legendIndex = [legendIndex[j]+(0.5*barWidth) if x != 0 else legendIndex[j] 
-        for j, x in enumerate(values)]
+  # Iterate through the data and plot the bar chart.
+  for result in results:
+    legendPosition = 0
+    legendBegin = nextBar
     
-  # Set the labels for the y-axis.
-  plt.ylabel('Seconds (lower is better)')
+    for i, data in enumerate(result):
+      # Use this variable to indicate if we have to store the bar handler.
+      check = 0
 
-  # Set the titel for the bar chart.
-  plt.title(chartName)
+      # The time value.
+      time = data[3]
+      # The name of the dataset.
+      dataset = data[8]
+
+      # Save the timing data for the timing table.
+      if dataset in timingData:
+        timingData[dataset][l] = time
+      else:
+        timingData[dataset] = ['-' for x in range(len(libraries))]
+        timingData[dataset][l] = time
+
+      # We can only plot scalar values so we job over the other.
+      if time == "failure":
+        failure += 1
+        continue
+      elif str(time).count(">") > 0:
+        timeouts += 1
+        continue      
+
+      totalTime += time
+
+      # Use the same color for the same dataset and save the timing to find 
+      # out the best time.
+      if dataset in color:
+        barColor = color[dataset]
+
+        # Use the time only if its lower then the old time value.
+        timming, lib = bestTiming[dataset]
+        if timming > time:
+          bestTiming[dataset] = (time, libraries[l])
+      else:
+        check = 1
+        barColor = colors[i%len(colors)]
+        color[dataset] = barColor
+        legendNames.append(dataset)
+
+        bestTiming[dataset] = (time, libraries[l])
+
+      # Plot the bar chart.
+      handler = plt.bar(nextBar, time, barWidth, alpha=opacity, color=barColor, 
+          label=dataset, fill=fill, lw=0.2)
+
+      # Increase the width for the next bar.
+      nextBar += barWidth
+      legendPosition += barWidth
+
+      # Save the bar handler for the legend.
+      if check == 1:
+        chartHandler.append(handler)
+
+    # Set the right lable postion in the legend.
+    legendIndex.append(legendBegin + (legendPosition / 2))
+    nextBar += (barWidth * 2)
+
+    # Next library for the next round.
+    l += 1
 
   # Set the labels for the x-axis.
-  plt.xticks(legendIndex , header)
+  plt.xticks(legendIndex , libraries)
+
+  # Set the color and the font of the x-axis and y-axis labels.
+  ax.tick_params(axis='both', which='major', labelsize=8, labelcolor="#6e6e6e")
+  ax.tick_params(axis='both', which='minor', labelsize=6, labelcolor="#6e6e6e")
 
   # Create the legend under the bar chart.
-  lgd = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), 
-      fancybox=True, shadow=True, ncol=8)
+  lgd = ax.legend(chartHandler, legendNames, loc='upper center', 
+    bbox_to_anchor=(0.5, 1.2), fancybox=True, shadow=False, ncol=8, fontsize=8)
+  lgd.get_frame().set_linewidth(0)
+  for label in lgd.get_texts():
+    label.set_color("#6e6e6e")
 
   # Save the bar chart.
-  fig.savefig(chartName, bbox_extra_artists=(lgd,), bbox_inches='tight', 
-      facecolor=fig.get_facecolor(), edgecolor='none')
+  fig.savefig(fileName, bbox_extra_artists=(lgd,), bbox_inches='tight', 
+    facecolor=fig.get_facecolor(), edgecolor='none', format='png')
+
+  # Count the time in which bestlib is the best.
+  bestLibCount = 0
+  for dataset, data in bestTiming.items():
+    if data[1] == bestlib:
+      bestLibCount += 1
+  
+  return (len(color), totalTime, failure, timeouts, bestLibCount, timingData)
 
 '''
 Generate a line chart with the specified informations.
 
-Data structure: dict( dict( list ) ) = { { [] } }
-
-@param data - Contains the values for the chart.
-@param chartName - Contains the name for the chart.
+@param data - List which contains the values for the line chart.
+@param fileName - The filename of the line chart.
 '''
-def GenerateLineChart(data, chartName):
+def GenerateSingleLineChart(data, fileName):
   # Bar chart settings.
-  lineWidth = 2
-  opacity = 0.8
-  windowWidth = 12
-  windowHeight = 8
+  lineWidth = 1.5
+  opacity = 0.9
+  windowWidth = 11
+  windowHeight = 1.5
   backgroundColor = '#F5F5F5'
+  gridLineWidth = 0.2
 
   # Create figure and set the color.
   matplotlib.rc('axes', facecolor=backgroundColor)
   fig = plt.figure(figsize=(windowWidth, windowHeight), 
-      facecolor=backgroundColor)
+      facecolor=backgroundColor, dpi=80)
   plt.rc('lines', linewidth=lineWidth)
   ax = plt.subplot(1,1,1)
 
-  # Data structure to temporary save the the correct color and the chart handler.
-  colorTemp = {}
-  legendChartHandler = []
-  legendName = []
+  # Set the grid style.
+  ax.yaxis.grid(True, linestyle='-', linewidth=gridLineWidth)
+  ax.xaxis.grid(False)
+  ax.spines['left'].set_visible(False)
+  ax.spines['top'].set_visible(False)
+  ax.spines['right'].set_visible(False)
+  ax.get_xaxis().tick_bottom()
+  ax.get_yaxis().tick_left()
+  ax.spines['bottom'].set_linewidth(gridLineWidth)
 
-  i = 0
-  for libaryName, timeSeries in data.items():
-    for name in timeSeries:
-      value = timeSeries[name]  # Timing data.
-      value = sorted(value, key=lambda tup: tup[0]) # Sort the timing data.
+  # Set ticks for the x-axis.
+  myLocator = mticker.MultipleLocator(1)
+  ax.xaxis.set_major_locator(myLocator)
 
-      # Normalize the timing data and generate the corresponding Z values.
-      Y = [float(x[1]) if isFloat(x[1]) else 0 for x in value] 
-      X = np.arange(len(Y))
+  # If we have only have a single value we don't want to start from zero so we 
+  # double the data.
+  if len(data) == 1:
+    data += data
+    
+  # Create the data for the x-axis.
+  X = np.arange(len(data))  
 
-      # Check if the graph has a specified color.
-      if name in colorTemp:
-        # Plot the line chrt.
-        plt.plot(X, Y, color=colorTemp[name], label=name, alpha=opacity)
-      else:
-        # Save the color to use it later for a graph with the same name.
-        color = colors[i%len(colors)]
-        # Save the bar handler to generate the legend.
-        handler, = plt.plot(X, Y, color=color, label=name, alpha=opacity)
-        colorTemp[name] = color
-        legendChartHandler.append(handler)
-        legendName.append(name)
-      i += 1
+  # Plot the line chart.
+  plt.plot(X, data, color=colors[0], alpha=opacity)
 
-  # Set the labels for the y-axis.
-  plt.ylabel('Seconds (lower is better)')
-
-  # Set the titel for the bar chart.
-  plt.title(chartName)
-
-  # Create the legend under the bar chart.
-  lgd = plt.legend(legendChartHandler, legendName, loc='upper center', 
-      bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=8)  
+  # Set the color and the font of the x-axis and y-axis labels.
+  ax.tick_params(axis='both', which='major', labelsize=8, labelcolor="#6e6e6e")
+  ax.tick_params(axis='both', which='minor', labelsize=6, labelcolor="#6e6e6e")
 
   # Save the line chart.
-  fig.savefig(chartName, bbox_extra_artists=(lgd,), bbox_inches='tight', 
-      facecolor=fig.get_facecolor(), edgecolor='none')
+  fig.savefig(fileName, bbox_inches='tight', facecolor=fig.get_facecolor(), 
+      edgecolor='none')
