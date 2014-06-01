3a68bc5e7b17683b52bcb3478e77ca4b79f115fd
Anand Soni <anand.92.soni@gmail.com>
2014-06-01 22:17:44 +0530

    One vs All for MCC and Lift added

--- methods/metrics/definitions.py
+++ methods/metrics/definitions.py
@@ -1,15 +1,15 @@
 '''
-    @file definitions.py
-    @author Anand Soni
+@file definitions.py
+@author Anand Soni
 
-    Implementation of various metrics common to all classifiers.
+Implementation of various metrics common to all classifiers.
 '''
 
 import re
 import sys
 
 class Metrics(object):
-	
+  
   '''
   @param labels - File containing true labels 
   @param prediction - File containing predicted labels 
@@ -20,8 +20,8 @@ class Metrics(object):
   '''
   @staticmethod 
   def ConfusionMatrix(labels, prediction):
-      from sklearn.metrics import confusion_matrix
-      return confusion_matrix(labels, prediction)
+    from sklearn.metrics import confusion_matrix
+    return confusion_matrix(labels, prediction)
  
   '''
   @param CM - The confusion matrix
@@ -31,14 +31,14 @@ class Metrics(object):
   '''
   @staticmethod
   def VisualizeConfusionMatrix(CM):
-      import pylab as pl
-      print(CM)
-      pl.matshow(CM)
-      pl.title('Confusion Matrix')
-      pl.colorbar()
-      pl.ylabel('True Label')
-      pl.xlabel('Predicted Label')
-      pl.show()
+    import pylab as pl
+    print(CM)
+    pl.matshow(CM)
+    pl.title('Confusion Matrix')
+    pl.colorbar()
+    pl.ylabel('True Label')
+    pl.xlabel('Predicted Label')
+    pl.show()
 
   '''
   @param CM - The confusion matrix
@@ -48,15 +48,15 @@ class Metrics(object):
   '''
   @staticmethod
   def AverageAccuracy(CM):
-      acc=0
-      Sum=0
-      l=len(CM)
-      for i in range(l):
-          for j in range(l):
-              Sum+=CM[i][j]
-          acc = acc + (CM[i][i]/Sum)*100
-          Sum=0
-      return acc/l
+    acc=0
+    Sum=0
+    l=len(CM)
+    for i in range(l):
+        for j in range(l):
+            Sum+=CM[i][j]
+        acc = acc + (CM[i][i]/Sum)*100
+        Sum=0
+    return acc/l
 
 
   '''
@@ -68,15 +68,15 @@ class Metrics(object):
   '''
   @staticmethod
   def PrecisionForAClass(class_i,CM):
-      l=len(CM)
-      truePositives = CM[class_i][class_i]
-      falsePositives = 0
-      for j in range(l):
-          falsePositives+=CM[j][class_i]
-      falsePositives-=truePositives
-      totalPositives = truePositives + falsePositives
-      precision = truePositives/totalPositives
-      return precision
+    l=len(CM)
+    truePositives = CM[class_i][class_i]
+    falsePositives = 0
+    for j in range(l):
+        falsePositives+=CM[j][class_i]
+    falsePositives-=truePositives
+    totalPositives = truePositives + falsePositives
+    precision = truePositives/totalPositives
+    return precision
 
   '''
   @param class_i - Index of the class in the confusion matrix
@@ -87,15 +87,15 @@ class Metrics(object):
   '''
   @staticmethod
   def RecallForAClass(class_i,CM):
-      l=len(CM)
-      truePositives = CM[class_i][class_i]
-      falseNegatives = 0
-      for j in range(l):
-          falseNegatives+=CM[class_i][j]
-      falseNegatives-=truePositives
-      total = truePositives + falseNegatives
-      recall = truePositives/total
-      return recall
+     l=len(CM)
+     truePositives = CM[class_i][class_i]
+     falseNegatives = 0
+     for j in range(l):
+         falseNegatives+=CM[class_i][j]
+     falseNegatives-=truePositives
+     total = truePositives + falseNegatives
+     recall = truePositives/total
+     return recall
 
   '''
   @param CM - The confusion matrix
@@ -107,21 +107,21 @@ class Metrics(object):
   '''
   @staticmethod
   def AvgPrecision(CM):
-      l=len(CM)
-      avgPrecision = 0
-      for i in range(l):
-          avgPrecision+=Metrics.PrecisionForAClass(i,CM)
-      avgPrecision = avgPrecision/l
-      return avgPrecision
+    l=len(CM)
+    avgPrecision = 0
+    for i in range(l):
+        avgPrecision+=Metrics.PrecisionForAClass(i,CM)
+    avgPrecision = avgPrecision/l
+    return avgPrecision
   
   @staticmethod
   def AvgRecall(CM):
-      l=len(CM)
-      avgRecall = 0
-      for i in range(l):
-          avgRecall+=Metrics.RecallForAClass(i,CM)
-      avgRecall = avgRecall/l
-      return avgRecall
+    l=len(CM)
+    avgRecall = 0
+    for i in range(l):
+        avgRecall+=Metrics.RecallForAClass(i,CM)
+    avgRecall = avgRecall/l
+    return avgRecall
 
   '''
   @param class_i - Index of the class in the confusion matrix
@@ -130,10 +130,11 @@ class Metrics(object):
   '''
   @staticmethod
   def FMeasureClass(class_i,CM):
-      precClass = Metrics.PrecisionForAClass(class_i,CM)
-      recClass = Metrics.RecallForAClass(class_i,CM)
-      fMeasure = 2*precClass*recClass/(precClass+recClass)
-      return fMeasure
+    precClass = Metrics.PrecisionForAClass(class_i,CM)
+    recClass = Metrics.RecallForAClass(class_i,CM)
+    fMeasure = 2*precClass*recClass/(precClass+recClass)
+    return fMeasure
+
 
   '''
   @param CM - The confusion matrix
@@ -142,12 +143,12 @@ class Metrics(object):
   '''
   @staticmethod
   def AvgFMeasure(CM):
-      l=len(CM)
-      avgF=0
-      for i in range(l):
-          avgF+=Metrics.FMeasureClass(i,CM)
-      avgF = avgF/l
-      return avgF
+    l=len(CM)
+    avgF=0
+    for i in range(l):
+        avgF+=Metrics.FMeasureClass(i,CM)
+    avgF = avgF/l
+    return avgF
    
   '''
   @param class_i - Index of the class in the confusion matrix
@@ -157,24 +158,43 @@ class Metrics(object):
   '''
   @staticmethod
   def LiftForAClass(class_i,CM):
-	  l=len(CM)
-	  #pgt - positives greater than threshold
-	  pgt = 0
-	  #tgt - total greater than threshold
-	  tgt = 0
-	  total = 0
-	  for i in range(l):
-		  pgt = pgt + CM[class_i][i]
-	  pgt = (1/pgt) * CM[class_i][class_i]
-	  for j in range(l):
-		  tgt = tgt + CM[j][0]
-	  for i in range(l):
-		  for j in range(l):
-			  total = total + CM[i][j]
-	  tgt = tgt/total
-	  lift = pgt/tgt
-	  return lift	   
+    l=len(CM)
+    #pgt - positives greater than threshold
+    pgt = 0
+    #tgt - total greater than threshold
+    tgt = 0
+    total = 0
+    for i in range(l):
+      pgt = pgt + CM[class_i][i]
+    pgt = (1/pgt) * CM[class_i][class_i]
+    for j in range(l):
+      tgt = tgt + CM[j][0]
+    for i in range(l):
+      for j in range(l):
+        total = total + CM[i][j]
+    tgt = tgt/total
+    lift = pgt/tgt
+    return lift	   
    
+  
+  '''
+  @param CM - The confusion matrix
+  Lift represents the ratio of the (%positives > threshold) to 
+  the (%total > threshold). Positive class decides the threshold.
+  To convert this binary class measure into multi-class measure, we
+  have used the LiftForAClass method to obtain Lifts for each class
+  applying the One vs All technique.
+  '''
+  @staticmethod
+  def LiftMultiClass(CM):
+    AvgLift=0
+    l=len(CM)
+    for i in range(l):
+      AvgLift+=LiftForAClass(i,CM)
+    AvgLift/=l
+    return AvgLift
+
+
   '''
   @param class_i - Index of the class in the confusion matrix
   @param CM - The confusion matrix
@@ -185,32 +205,51 @@ class Metrics(object):
   '''
   @staticmethod 		
   def MatthewsCorrelationCoefficientClass(class_i, CM):
-	  import math	
-	  l=len(CM)
-	  truePositives = CM[class_i][class_i]
-	  falsePositives = 0
-	  for j in range(l):
-		  falsePositives+=CM[j][class_i]
-	  falsePositives-=truePositives 
-	  falseNegatives=0
-	  for j in range(l):
-		  falseNegatives+=CM[class_i][j]
-	  falseNegatives-=truePositives
-	  trueNegatives=0
-	  #calculate trueNegatives
-	  for i in range(l):
-		  if i!=class_i:
-			  for j in range(l):
-				  trueNegatives+=CM[i][j]
-			  trueNegatives-=CM[i][class_i]
-	  Numerator = (truePositives*trueNegatives) - (falsePositives*falseNegatives)
-	  Denominator=math.sqrt((truePositives + falsePositives)*
+    import math	
+    l=len(CM)
+    truePositives = CM[class_i][class_i]
+    falsePositives = 0
+    for j in range(l):
+      falsePositives+=CM[j][class_i]
+    falsePositives-=truePositives 
+    falseNegatives=0
+    for j in range(l):
+      falseNegatives+=CM[class_i][j]
+    falseNegatives-=truePositives
+    trueNegatives=0
+    #calculate trueNegatives
+    for i in range(l):
+      if i!=class_i:
+        for j in range(l):
+          trueNegatives+=CM[i][j]
+        trueNegatives-=CM[i][class_i]
+    Numerator = (truePositives*trueNegatives) - (falsePositives*falseNegatives)
+    Denominator=math.sqrt((truePositives + falsePositives)*
 							(truePositives + falseNegatives)*
 							(trueNegatives + falsePositives)*
 							(trueNegatives + falseNegatives))
-	  MCC = Numerator/Denominator
-	  return MCC
+    MCC = Numerator/Denominator
+    return MCC
 	   	
+
+  '''
+  @param CM - The confusion matrix
+  MCC is a balanced measure which returns values between +1 and -1.
+  A coefficient of +1 represents a perfect prediction, 0 no better 
+  than random prediction and −1 indicates total disagreement between 
+  prediction and observation. We use the MCC for a single class as
+  obtained by applying the One vs All approach in the above method
+  below.
+  '''
+  @staticmethod
+  def MCCMultiClass(CM):
+    MCC=0
+    l=len(CM)
+    for i in range(l):
+      MCC+=MatthewsCorrelationCoefficientClass(i,CM)
+    MCC/=l
+    return MCC
+
   '''
   @param truelabelFile - Name of the file which contains the true label
   for the instance
@@ -222,41 +261,41 @@ class Metrics(object):
   '''
   @staticmethod 		
   def MeanSquaredError(truelabelFile, probabilities, CM):
-	  import numpy as np
-	  import math
-	  #l : Number of classes
-	  l=len(CM)
-	  #trueVec : Vector/list with trueVec[index]=1 for the true class, 0 otherwise
-	  Vec = np.genfromtxt(truelabelFile,delimiter=',')
-	  instances=len(Vec)
-	  trueVec=[]
-	  for i in range(instances):
-		  vec=[]
-		  for j in range(l):
-			  vec.append(0)
-		  vec[int(Vec[i])-1]=1
-		  trueVec.append(vec)
-	  #trueArray : 2D numpy array after converting trueVec
-	  trueArray=np.array(trueVec)
-	  #probVec : 2D numpy array with trueVec[index]=probability for the instance
-	  #to be in that class.
-	  probVec = np.genfromtxt(probabilities,delimiter=',')
-	  diffArray = trueArray - probVec
-	  #Quadratic Loss Function
-	  quadraticLoss=[]
-	  for i in range(len(diffArray)):
-		  squaredloss=0
-		  for j in range(len(diffArray[i])):
-			  squaredloss+=diffArray[i][j]*diffArray[i][j]
-		  quadraticLoss.append(squaredloss)
-	  quadraticLoss=np.array(quadraticLoss)
-	  #Divide the total squared loss for each instance by the number of classes
-	  quadraticLoss = quadraticLoss/l
-	  totalLoss=0
-	  for i in range(len(quadraticLoss)):
-		  totalLoss+=quadraticLoss[i]
-	  totalLoss = totalLoss/instances
-	  return totalLoss
+    import numpy as np
+    import math
+    #l : Number of classes
+    l=len(CM)
+    #trueVec : Vector/list with trueVec[index]=1 for the true class, 0 otherwise
+    Vec = np.genfromtxt(truelabelFile,delimiter=',')
+    instances=len(Vec)
+    trueVec=[]
+    for i in range(instances):
+      vec=[]
+      for j in range(l):
+        vec.append(0)
+        vec[int(Vec[i])-1]=1
+        trueVec.append(vec)
+    #trueArray : 2D numpy array after converting trueVec
+    trueArray=np.array(trueVec)
+    #probVec : 2D numpy array with trueVec[index]=probability for the instance
+    #to be in that class.
+    probVec = np.genfromtxt(probabilities,delimiter=',')
+    diffArray = trueArray - probVec
+    #Quadratic Loss Function
+    quadraticLoss=[]
+    for i in range(len(diffArray)):
+      squaredloss=0
+      for j in range(len(diffArray[i])):
+        squaredloss+=diffArray[i][j]*diffArray[i][j]
+        quadraticLoss.append(squaredloss)
+    quadraticLoss=np.array(quadraticLoss)
+	#Divide the total squared loss for each instance by the number of classes     
+    quadraticLoss = quadraticLoss/l
+    totalLoss=0
+    for i in range(len(quadraticLoss)):
+      totalLoss+=quadraticLoss[i]
+    totalLoss = totalLoss/instances
+    return totalLoss
 		   
   
   '''
@@ -270,15 +309,15 @@ class Metrics(object):
   '''
   @staticmethod 		
   def MeanPredictiveInformation(truelabels, predictedlabels):
-	  import numpy as np
-	  import math
-	  predicted=np.genfromtxt(predictedlabels, delimiter=',')
-	  actual=np.genfromtxt(truelabels, delimiter=',')
-	  instances=len(actual)
-	  predictiveSum=0
-	  for i in range(instances):
-		  predictiveSum+=((actual[i] * math.log(predicted[i],2))+
+    import numpy as np
+    import math
+    predicted=np.genfromtxt(predictedlabels, delimiter=',')
+    actual=np.genfromtxt(truelabels, delimiter=',')
+    instances=len(actual)
+    predictiveSum=0
+    for i in range(instances):
+      predictiveSum+=((actual[i] * math.log(predicted[i],2))+
 						  ((1-actual[i]) * math.log(1-predicted[i],2)))
-	  predictiveSum/=instances
-	  return predictiveSum	  
+    predictiveSum/=instances
+    return predictiveSum	  
 		
