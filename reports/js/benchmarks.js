45c475216734efb59336120412cbe444df0b201c
Ryan Curtin <ryan@ratml.org>
2014-10-06 16:46:48 -0400

    A few more changes.

--- reports/js/benchmarks.js
+++ reports/js/benchmarks.js
@@ -57,7 +57,7 @@ function clearChart()
 {
   if (activeChartType != null)
   {
-    activeChartType.clear();
+    activeChartType.clearChart();
   }
 }
 
@@ -100,8 +100,11 @@ function chartTypeSelect()
   }
 
   // Ditch whatever's there.
-  clearChart();
-  clearMethodControl();
+  if (activeChartType != null)
+  {
+    activeChartType.clear();
+  }
+
   var selectHolder = d3.select(".selectholder");
   selectHolder.selectAll('label').remove();
   selectHolder.selectAll('select').remove();
@@ -110,86 +113,7 @@ function chartTypeSelect()
   if (chartType == "algorithm-parameter-comparison") { activeChartType = rc; }
   else if (chartType == "historical-comparison") { activeChartType = hc; }
   else if (chartType == "metric-comparison") { activeChartType = mc; }
-  //else if (chartType == "dataset-comparison")
+  else if (chartType == "dataset-comparison") { activeChartType = dc; }
 
   activeChartType.onTypeSelect();
-}
-
-function listMainDatasets()
-{
-  // We want the list of methods and dataset combinations.  Then we will figure
-  // out how many methods go with each dataset.  For now though I'll just get a
-  // list of datasets...
-//  var sqlstr = "SELECT DISTINCT datasets.name, methods.id, methods.name, methods.parameters from methods, datasets, results where results.dataset_id == datasets.id and results.method_id == methods.id group by results.build_id;";
-  var sqlstr = "SELECT datasets.name FROM datasets;";
-  results = db.exec(sqlstr);
-
-  var dataset_select_box = document.getElementById("main_dataset_select");
-  for (i = dataset_select_box.options.length - 1; i >= 0; i--)
-  {
-    dataset_select_box.options[i] = null;
-  }
-  for (i = 0; i < results[0].values.length; i++)
-  {
-    var new_option = document.createElement("option");
-    new_option.text = results[0].values[i][0];
-    dataset_select_box.add(new_option);
-  }
-  dataset_select_box.selectedIndex = -1;
-}
-
-function mainDatasetSelect()
-{
-  var dataset_select_box = document.getElementById("main_dataset_select");
-  dataset_name = dataset_select_box.options[dataset_select_box.selectedIndex].text;
-
-  // Create an empty chart.
-  clearChart();
-  clearMethodControl();
-  buildChart();
-
-  // Now create the legend at the bottom that will allow us to add/remove
-  // methods.
-  d3.selectAll(".legendholder").append("div").attr("class", "methodcontrol");
-
-  d3.selectAll(".legendholder").append("input")
-      .attr("type", "button")
-      .attr("class", "add_method_button")
-      .attr("onclick", "clickAddButton()")
-      .attr("value", "Add another method");
-  d3.selectAll(".legendholder").append("input")
-      .attr("type", "button")
-      .attr("class", "clear_methods_button")
-      .attr("onclick", "clickClearMethods()")
-      .attr("value", "Remove all methods");
-  d3.selectAll(".legendholder").append("input")
-      .attr("type", "button")
-      .attr("class", "redraw_methods_button")
-      .attr("onclick", "clickRedrawMethods()")
-      .attr("value", "Redraw graph");
-
-  control_list_length = 0;
-
-  // Collect the results for lists of methods.
-}
-
-function clickAddButton()
-{
-  var newmethodcontrol = d3.selectAll(".methodcontrol").append("div").attr("class", "methodcontroldiv");
-
-  newmethodcontrol.append("label")
-      .attr("for", "method_select_" + String(control_list_length))
-      .attr("class", "method-select-label")
-      .text("Method:");
-  newmethodcontrol.append("select")
-      .attr("id", "method_select_" + String(control_list_length))
-      .attr("onchange", "methodControlListSelect()");
-  newmethodcontrol.append("label")
-      .attr("for", "param_select_" + String(control_list_length))
-      .attr("class", "param-select-label")
-      .text("Parameters:");
-  newmethodcontrol.append("select")
-      .attr("id", "param_select_" + String(control_list_length));
-
-  control_list_length++;
 }
