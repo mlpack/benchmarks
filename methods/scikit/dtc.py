cf5bff0c286b2243d97b57c9c60e260cc9065a20
Marcus Edel <marcus.edel@fu-berlin.de>
2014-11-17 20:54:34 +0100

    Add Decision Tree Classifier benchmark script (scikit).

---
+++ methods/scikit/dtc.py
@@ -0,0 +1,151 @@
+'''
+  @file dtc.py
+  @author Marcus Edel
+
+  Decision Tree Classifier with scikit.
+'''
+
+import os
+import sys
+import inspect
+
+# Import the util path, this method even works if the path contains symlinks to
+# modules.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../util")))
+if cmd_subfolder not in sys.path:
+  sys.path.insert(0, cmd_subfolder)
+
+#Import the metrics definitions path.
+metrics_folder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../metrics")))
+if metrics_folder not in sys.path:
+  sys.path.insert(0, metrics_folder)  
+
+from log import *
+from timer import *
+from definitions import *
+from misc import *
+
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+'''
+This class implements the Decision Tree Classifier benchmark.
+'''
+class DTC(object):
+
+  ''' 
+  Create the Decision Tree Classifier benchmark instance.
+  
+  @param dataset - Input dataset to perform DTC on.
+  @param timeout - The time until the timeout. Default no timeout.
+  @param verbose - Display informational messages.
+  '''
+  def __init__(self, dataset, timeout=0, verbose=True):
+    self.verbose = verbose
+    self.dataset = dataset
+    self.timeout = timeout
+    self.model = None
+    self.criterion = 'gini'
+    self.max_depth = None
+    self.seed = 0
+
+  '''
+  Build the model for the Decision Tree Classifier.
+
+  @param data - The train data.
+  @param labels - The labels for the train set.
+  @return The created model.
+  '''
+  def BuildModel(self, data, labels):
+    # Create and train the classifier.
+    dtc = DecisionTreeClassifier(criterion=self.criterion,
+                                 max_depth=self.max_depth,
+                                 random_state=self.seed)
+    dtc.fit(data, labels)
+    return dtc
+
+  '''
+  Use the scikit libary to implement the Decision Tree Classifier.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not 
+  successful.
+  '''
+  def DTCScikit(self, options):
+    def RunDTCScikit(q):
+      totalTimer = Timer()
+      
+      Log.Info("Loading dataset", self.verbose)
+      trainData, labels = SplitTrainData(self.dataset)
+      testData = LoadDataset(self.dataset[1])
+
+      # Get all the parameters.
+      c = re.search("-c (\s+)", options)
+      d = re.search("-d (\s+)", options)
+      s = re.search("-n (\d+)", options)
+      
+      self.criterion = 'gini' if not c else str(c.group(1))
+      self.max_depth = None if not d else int(d.group(1))
+      self.seed = 0 if not s else int(s.group(1))
+
+      try:
+        with totalTimer:
+          self.model = self.BuildModel(trainData, labels)
+          # Run Decision Tree Classifier on the test dataset.
+          self.model.predict(testData)
+      except Exception as e:
+        Log.Debug(str(e))
+        q.put(-1)
+        return -1
+
+      time = totalTimer.ElapsedTime()
+      q.put(time)
+
+      return time
+
+    return timeout(RunDTCScikit, self.timeout)
+
+  '''
+  Perform the Decision Tree Classifier. If the method has been 
+  successfully completed return the elapsed time in seconds.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not 
+  successful.
+  '''
+  def RunTiming(self, options):
+    Log.Info("Perform DTC.", self.verbose)
+
+    if len(self.dataset) >= 2:
+      return self.DTCScikit(options)
+    else:
+      Log.Fatal("This method requires two datasets.")      
+
+  def RunMetrics(self, options):
+    if len(self.dataset) >= 3:
+
+      # Check if we need to create a model.
+      if not self.model:
+        trainData, labels = SplitTrainData(self.dataset)
+        self.model = self.BuildModel(trainData, labels)
+
+      testData = LoadDataset(self.dataset[1])
+      truelabels = LoadDataset(self.dataset[2])
+      
+      predictedlabels = self.model.predict(testData)
+
+      # Datastructure to store the results.
+      metrics = {}
+
+      confusionMatrix = Metrics.ConfusionMatrix(truelabels, predictedlabels)
+      metrics['ACC'] = Metrics.AverageAccuracy(confusionMatrix)
+      metrics['MCC'] = Metrics.MCCMultiClass(confusionMatrix)
+      metrics['Precision'] = Metrics.AvgPrecision(confusionMatrix)
+      metrics['Recall'] = Metrics.AvgRecall(confusionMatrix)
+      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, predictedlabels)
+      return metrics
+
+    else:
+      Log.Fatal("This method requires three datasets.")
