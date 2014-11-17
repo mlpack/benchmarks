ca1eee081903ca3973c3ceca7683d1ca7d264bd2
Marcus Edel <marcus.edel@fu-berlin.de>
2014-11-17 23:23:00 +0100

    Add k-nearest neighbors vote benchmark script (mlpy).

---
+++ methods/mlpy/knc.py
@@ -0,0 +1,149 @@
+'''
+  @file knc.py
+  @author Marcus Edel
+
+  Classifier implementing the k-nearest neighbors vote with mlpy.
+'''
+
+import os
+import sys
+import inspect
+
+# Import the util path, this method even works if the path contains symlinks to
+# modules.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../util")))
+if cmd_subfolder not in sys.path:
+  sys.path.insert(0, cmd_subfolder)
+
+#Import the metrics definitions path.
+metrics_folder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../metrics")))
+if metrics_folder not in sys.path:
+  sys.path.insert(0, metrics_folder)  
+
+from log import *
+from timer import *
+from definitions import *
+from misc import *
+
+import numpy as np
+import mlpy
+
+'''
+This class implements the k-nearest neighbors Classifier benchmark.
+'''
+class KNC(object):
+
+  ''' 
+  Create the k-nearest neighbors Classifier benchmark instance.
+  
+  @param dataset - Input dataset to perform KNC on.
+  @param timeout - The time until the timeout. Default no timeout.
+  @param verbose - Display informational messages.
+  '''
+  def __init__(self, dataset, timeout=0, verbose=True):
+    self.verbose = verbose
+    self.dataset = dataset
+    self.timeout = timeout
+    self.model = None
+    self.n_neighbors = 5
+    self.algorithm = 'kd_tree'
+    self.leaf_size = 30
+    self.metric = 'minkowski'
+
+  '''
+  Build the model for the k-nearest neighbors Classifier.
+
+  @param data - The train data.
+  @param labels - The labels for the train set.
+  @return The created model.
+  '''
+  def BuildModel(self, data, labels):
+    # Create and train the classifier.
+    knc = mlpy.KNN(k=self.n_neighbors)
+    knc.learn(data, labels)
+    return knc
+
+  '''
+  Use the mlpy libary to implement the k-nearest neighbors Classifier.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not 
+  successful.
+  '''
+  def KNCMlpy(self, options):
+    def RunKNCMlpy(q):
+      totalTimer = Timer()
+      
+      Log.Info("Loading dataset", self.verbose)
+      trainData, labels = SplitTrainData(self.dataset)
+      testData = LoadDataset(self.dataset[1])
+
+      # Get all the parameters.
+      n = re.search("-n (\d+)", options)
+      a = re.search("-a (\s+)", options)
+      l = re.search("-l (\d+)", options)
+      m = re.search("-m (\s+)", options)
+
+      self.n_neighbors = 5 if not n else int(n.group(1))
+
+      try:
+        with totalTimer:
+          self.model = self.BuildModel(trainData, labels)
+          # Run k-nearest neighbors Classifier on the test dataset.
+          self.model.pred(testData)
+      except Exception as e:
+        Log.Debug(str(e))
+        q.put(-1)
+        return -1
+
+      time = totalTimer.ElapsedTime()
+      q.put(time)
+
+      return time
+
+    return timeout(RunKNCMlpy, self.timeout)
+
+  '''
+  Perform the k-nearest neighbors Classifier. If the method has been 
+  successfully completed return the elapsed time in seconds.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not 
+  successful.
+  '''
+  def RunTiming(self, options):
+    Log.Info("Perform KNC.", self.verbose)
+
+    if len(self.dataset) >= 2:
+      return self.KNCMlpy(options)
+    else:
+      Log.Fatal("This method requires two datasets.")      
+
+  def RunMetrics(self, options):
+    if len(self.dataset) >= 3:
+
+      # Check if we need to create a model.
+      if not self.model:
+        trainData, labels = SplitTrainData(self.dataset)
+        self.model = self.BuildModel(trainData, labels)
+
+      testData = LoadDataset(self.dataset[1])
+      truelabels = LoadDataset(self.dataset[2])
+      
+      predictedlabels = self.model.pred(testData)
+
+      # Datastructure to store the results.
+      metrics = {}
+
+      confusionMatrix = Metrics.ConfusionMatrix(truelabels, predictedlabels)
+      metrics['ACC'] = Metrics.AverageAccuracy(confusionMatrix)
+      metrics['MCC'] = Metrics.MCCMultiClass(confusionMatrix)
+      metrics['Precision'] = Metrics.AvgPrecision(confusionMatrix)
+      metrics['Recall'] = Metrics.AvgRecall(confusionMatrix)
+      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, predictedlabels)
+      return metrics
+
+    else:
+      Log.Fatal("This method requires three datasets.")
---
+++ methods/mlpy/perceptron.py
@@ -0,0 +1,152 @@
+'''
+  @file perceptron.py
+  @author Marcus Edel
+
+  Perceptron Classifier with mlpy.
+'''
+
+import os
+import sys
+import inspect
+
+# Import the util path, this method even works if the path contains symlinks to
+# modules.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../util")))
+if cmd_subfolder not in sys.path:
+  sys.path.insert(0, cmd_subfolder)
+
+#Import the metrics definitions path.
+metrics_folder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../metrics")))
+if metrics_folder not in sys.path:
+  sys.path.insert(0, metrics_folder)  
+
+from log import *
+from timer import *
+from definitions import *
+from misc import *
+
+import numpy as np
+import mlpy
+
+'''
+This class implements the Perceptron benchmark.
+'''
+class PERCEPTRON(object):
+
+  ''' 
+  Create the Perceptron benchmark instance.
+  
+  @param dataset - Input dataset to perform the Perceptron Classifier on.
+  @param timeout - The time until the timeout. Default no timeout.
+  @param verbose - Display informational messages.
+  '''
+  def __init__(self, dataset, timeout=0, verbose=True):
+    self.verbose = verbose
+    self.dataset = dataset
+    self.timeout = timeout
+    self.model = None
+    self.iterations = 1000
+
+  '''
+  Build the model for the Perceptron.
+
+  @param data - The train data.
+  @param responses - The responses for the training set.
+  @return The created model.
+  '''
+  def BuildModel(self, data, responses):
+    # Create and train the classifier.
+    model = mlpy.Perceptron(alpha=0.1,
+                            thr=0.05,
+                            maxiters=self.iterations)
+    model.learn(data, responses)
+    return model
+
+  '''
+  Use the mlpy libary to implement the Perceptron.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not 
+  successful.
+  '''
+  def PerceptronMlpy(self, options):
+    def RunPerceptronMlpy(q):
+      totalTimer = Timer()
+
+      # Load input dataset.
+      # If the dataset contains two files then the second file is the test file.
+      Log.Info("Loading dataset", self.verbose)
+      if len(self.dataset) >= 2:
+        testSet = LoadDataset(self.dataset[1])
+      else:
+        Log.Fatal("This method requires atleast two datasets.")
+
+      # Use the last row of the training set as the responses.
+      X, y = SplitTrainData(self.dataset)
+
+      i = re.search('-i (\d+)', options)
+      self.iterations = 1000 if not i else int(i.group(1))
+
+      try:
+        with totalTimer:
+          # # Perform perceptron classification.
+          self.model = self.BuildModel(X, y)
+
+          if len(self.dataset) >= 2:
+            #prediction on the test data.
+            pred = model.pred(testSet)
+      except Exception as e:
+        q.put(-1)
+        return -1
+
+      time = totalTimer.ElapsedTime()
+      q.put(time)
+
+      return time
+
+    return timeout(RunPerceptronMlpy, self.timeout)
+
+  '''
+  Perform the Perceptron Classifier. If the method has been successfully
+  completed return the elapsed time in seconds.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not 
+  successful.
+  '''
+  def RunTiming(self, options):
+    Log.Info("Perform Perceptron.", self.verbose)
+
+    return self.PerceptronMlpy(options)
+
+  '''
+  Run all the metrics for the classifier.  
+  '''  
+  def RunMetrics(self, options):
+    if len(self.dataset) >= 2:
+
+      # Check if we need to create a model.
+      if not self.model:
+        trainData, labels = SplitTrainData(self.dataset)
+        self.model = self.BuildModel(trainData, labels)
+
+      testData = LoadDataset(self.dataset[1])
+      truelabels = LoadDataset(self.dataset[2])
+
+      predictedlabels = self.model.pred(testData)
+
+      # Datastructure to store the results.
+      metrics = {}
+
+      confusionMatrix = Metrics.ConfusionMatrix(truelabels, predictedlabels)
+      metrics['ACC'] = Metrics.AverageAccuracy(confusionMatrix)
+      metrics['MCC'] = Metrics.MCCMultiClass(confusionMatrix)
+      metrics['Precision'] = Metrics.AvgPrecision(confusionMatrix)
+      metrics['Recall'] = Metrics.AvgRecall(confusionMatrix)
+      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, predictedlabels)
+      return metrics
+    else:
+      Log.Warn("This method requires three datasets.")
+      return None
