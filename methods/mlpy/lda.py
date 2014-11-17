350862aed10c9610a4669f816fd1accf73e8e2cf
Marcus Edel <marcus.edel@fu-berlin.de>
2014-11-17 23:12:42 +0100

    Add Linear Discriminant Analysis benchmark script (mlpy).

---
+++ methods/mlpy/lda.py
@@ -0,0 +1,137 @@
+'''
+  @file lda.py
+  @author Marcus Edel
+
+  Linear Discriminant Analysis with mlpy.
+'''
+
+import os
+import sys
+import inspect
+
+# Import the util path, this method even works if the path contains symlinks to
+# modules.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../util")))
+if cmd_subfolder not in sys.path:
+  sys.path.insert(0, cmd_subfolder)
+
+#Import the metrics definitions path.
+metrics_folder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../metrics")))
+if metrics_folder not in sys.path:
+  sys.path.insert(0, metrics_folder)  
+
+from log import *
+from timer import *
+from definitions import *
+from misc import *
+
+import numpy as np
+import mlpy
+
+'''
+This class implements the Linear Discriminant Analysis benchmark.
+'''
+class LDA(object):
+
+  ''' 
+  Create the Linear Discriminant Analysis benchmark instance.
+  
+  @param dataset - Input dataset to perform LDA on.
+  @param timeout - The time until the timeout. Default no timeout.
+  @param verbose - Display informational messages.
+  '''
+  def __init__(self, dataset, timeout=0, verbose=True):
+    self.verbose = verbose
+    self.dataset = dataset
+    self.timeout = timeout
+    self.model = None
+
+  '''
+  Build the model for the Linear Discriminant Analysis.
+
+  @param data - The train data.
+  @param labels - The labels for the train set.
+  @return The created model.
+  '''
+  def BuildModel(self, data, labels):
+    # Create and train the classifier.
+    lda = mlpy.LDAC()
+    lda.learn(data, labels)
+    return lda
+
+  '''
+  Use the mlpy libary to implement the Linear Discriminant Analysis.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not 
+  successful.
+  '''
+  def LDAMlpy(self, options):
+    def RunLDAMlpy(q):
+      totalTimer = Timer()
+      
+      Log.Info("Loading dataset", self.verbose)
+      trainData, labels = SplitTrainData(self.dataset)
+      testData = LoadDataset(self.dataset[1])
+
+      try:
+        with totalTimer:
+          self.model = self.BuildModel(trainData, labels)
+          # Run Linear Discriminant Analysis on the test dataset.
+          self.model.pred(testData)
+      except Exception as e:
+        Log.Debug(str(e))
+        q.put(-1)
+        return -1
+
+      time = totalTimer.ElapsedTime()
+      q.put(time)
+
+      return time
+
+    return timeout(RunLDAMlpy, self.timeout)
+
+  '''
+  Perform the Linear Discriminant Analysis. If the method has been 
+  successfully completed return the elapsed time in seconds.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not 
+  successful.
+  '''
+  def RunTiming(self, options):
+    Log.Info("Perform LDA.", self.verbose)
+
+    if len(self.dataset) >= 2:
+      return self.LDAMlpy(options)
+    else:
+      Log.Fatal("This method requires two datasets.")      
+
+  def RunMetrics(self, options):
+    if len(self.dataset) >= 3:
+
+      # Check if we need to create a model.
+      if not self.model:
+        trainData, labels = SplitTrainData(self.dataset)
+        self.model = self.BuildModel(trainData, labels)
+
+      testData = LoadDataset(self.dataset[1])
+      truelabels = LoadDataset(self.dataset[2])
+      
+      predictedlabels = self.model.pred(testData)
+
+      # Datastructure to store the results.
+      metrics = {}
+
+      confusionMatrix = Metrics.ConfusionMatrix(truelabels, predictedlabels)
+      metrics['ACC'] = Metrics.AverageAccuracy(confusionMatrix)
+      metrics['MCC'] = Metrics.MCCMultiClass(confusionMatrix)
+      metrics['Precision'] = Metrics.AvgPrecision(confusionMatrix)
+      metrics['Recall'] = Metrics.AvgRecall(confusionMatrix)
+      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, predictedlabels)
+      return metrics
+
+    else:
+      Log.Fatal("This method requires three datasets.")
