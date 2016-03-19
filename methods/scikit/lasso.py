29c1572d1f6ed6e1406a5fdbc8a4d4396ffe1afc
youssef <youssef.emad.attia@gmail.com>
2016-03-19 21:31:14 +0200

    add lasso for shogun and scikit

---
+++ methods/scikit/lasso.py
@@ -0,0 +1,93 @@
+'''
+  @file lasso.py
+  @author Youssef Emad El-Din
+
+  Lasso Regression with scikit.
+'''
+
+import os
+import sys
+import inspect
+
+# Import the util path, this method even works if the path contains symlinks to
+# modules.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../util")))
+if cmd_subfolder not in sys.path:
+  sys.path.insert(0, cmd_subfolder)
+
+from log import *
+from timer import *
+
+import numpy as np
+from sklearn.linear_model import Lasso
+
+'''
+This class implements the Lasso Regression benchmark.
+'''
+class LASSO(object):
+
+  '''
+  Create the Lasso Regression benchmark instance.
+
+  @param dataset - Input dataset to perform Least Angle Regression on.
+  @param timeout - The time until the timeout. Default no timeout.
+  @param verbose - Display informational messages.
+  '''
+  def __init__(self, dataset, timeout=0, verbose=True):
+    self.verbose = verbose
+    self.dataset = dataset
+    self.timeout = timeout
+
+  '''
+  Use the scikit libary to implement Lasso Regression.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not
+  successful.
+  '''
+  def LASSOScikit(self, options):
+    def RunLASSOScikit(q):
+      totalTimer = Timer()
+
+      # Load input dataset.
+      Log.Info("Loading dataset", self.verbose)
+      inputData = np.genfromtxt(self.dataset[0], delimiter=',')
+      responsesData = np.genfromtxt(self.dataset[1], delimiter=',')
+
+      # Get all the parameters.
+      lambda1 = re.search("-l (\d+)", options)
+      lambda1 = 0.0 if not lambda1 else int(lambda1.group(1))
+          
+      try:
+        with totalTimer:
+          # Perform LASSO.
+          model = Lasso()
+          model.fit(inputData, responsesData)
+          out = model.coef_
+      except Exception as e:
+        q.put(-1)
+        return -1
+
+      time = totalTimer.ElapsedTime()
+      q.put(time)
+      return time
+
+    return timeout(RunLASSOScikit, self.timeout)
+
+  '''
+  Perform Lasso Regression. If the method has been successfully completed
+  return the elapsed time in seconds.
+
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not
+  successful.
+  '''
+  def RunTiming(self, options):
+    Log.Info("Perform LASSO.", self.verbose)
+
+    if len(self.dataset) != 2:
+      Log.Fatal("This method requires two datasets.")
+      return -1
+
+    return self.LASSOScikit(options)
