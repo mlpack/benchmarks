501c913f62bc3fd2ee184ca40dfe92c4971bbdcd
youssef <youssef.emad.attia@gmail.com>
2016-03-22 15:32:33 +0200

    add qda benchmark for shogun

---
+++ tests/benchmark_qda.py
@@ -0,0 +1,86 @@
+'''
+  @file benchmark_qda.py
+  @author Youssef Emad El-Din
+  Test for the QDA scripts.
+'''
+
+import unittest
+
+import os, sys, inspect
+
+# Import the util path, this method even works if the path contains
+# symlinks to modules.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], '../util')))
+if cmd_subfolder not in sys.path:
+  sys.path.insert(0, cmd_subfolder)
+
+from loader import *
+
+'''
+Test the shogun QDA script.
+'''
+class QDA_SHOGUN_TEST(unittest.TestCase):
+
+  '''
+  Test initialization.
+  '''
+  def setUp(self):
+    self.dataset = ['datasets/abalone7_train.csv', 'datasets/abalone7_test.csv', 'datasets/abalone7_labels.csv']
+    self.verbose = False
+    self.timeout = 9000
+
+    module = Loader.ImportModuleFromPath("methods/shogun/qda.py")
+    obj = getattr(module, "QDA")
+    self.instance = obj(self.dataset, verbose=self.verbose, timeout=self.timeout)
+
+  '''
+  Test the constructor.
+  '''
+  def test_Constructor(self):
+    self.assertEqual(self.instance.verbose, self.verbose)
+    self.assertEqual(self.instance.timeout, self.timeout)
+    self.assertEqual(self.instance.dataset, self.dataset)
+
+  '''
+  Test the 'RunTiming' function.
+  '''
+  def test_RunTiming(self):
+    result = self.instance.RunTiming("")
+    self.assertTrue(result > 0)
+
+'''
+Test the scikit QDA script.
+'''
+class QDA_SCIKIT_TEST(unittest.TestCase):
+
+  '''
+  Test initialization.
+  '''
+  def setUp(self):
+    self.dataset = ['datasets/abalone7_train.csv', 'datasets/abalone7_test.csv', 'datasets/abalone7_labels.csv']
+    self.verbose = False
+    self.timeout = 9000
+
+    module = Loader.ImportModuleFromPath("methods/scikit/qda.py")
+    obj = getattr(module, "QDA")
+    self.instance = obj(self.dataset, verbose=self.verbose, timeout=self.timeout)
+
+  '''
+  Test the constructor.
+  '''
+  def test_Constructor(self):
+    self.assertEqual(self.instance.verbose, self.verbose)
+    self.assertEqual(self.instance.timeout, self.timeout)
+    self.assertEqual(self.instance.dataset, self.dataset)
+
+  '''
+  Test the 'RunTiming' function.
+  '''
+  def test_RunTiming(self):
+    result = self.instance.RunTiming("")
+    self.assertTrue(result > 0)
+
+
+if __name__ == '__main__':
+  unittest.main()
