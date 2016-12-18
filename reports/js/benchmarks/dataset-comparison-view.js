56005766ed68575bed1720c37dedd697c17cf4d2
Marcus Edel <marcus.edel@fu-berlin.de>
2016-12-18 21:42:22 +0100

    Refactor the dataset comparison view to work with sqlite and mysql.

--- reports/js/benchmarks/dataset-comparison-view.js
+++ reports/js/benchmarks/dataset-comparison-view.js
@@ -24,15 +24,16 @@ dc.onTypeSelect = function()
 // List the datasets.
 dc.listDatasets = function()
 {
-  var sqlstr = "SELECT datasets.name FROM datasets;";
-  var results = db.exec(sqlstr);
+  var sqlstr = "SELECT datasets.name as dataset FROM datasets;";
+  var results = dbExec(sqlstr);
+  results = dbType === "sqlite" ? results[0].values : results;
 
   var dataset_select_box = document.getElementById("main_dataset_select");
   clearSelectBox(dataset_select_box);
-  for (i = 0; i < results[0].values.length; i++)
+  for (i = 0; i < results.length; i++)
   {
     var new_option = document.createElement("option");
-    new_option.text = results[0].values[i][0];
+    new_option.text = dbType === "sqlite" ? results[i][0] : results[i].dataset;
     dataset_select_box.add(new_option);
   }
   dataset_select_box.selectedIndex = -1;
@@ -46,9 +47,6 @@ dc.datasetSelect = function()
 
   // Create an empty chart.
   dc.clear();
-//  dc.clearChart();
-//  dc.clearMethodControl();
-//  dc.buildChart();
 
   // Now create the legend at the bottom that will allow us to add/remove
   // methods.
@@ -73,8 +71,9 @@ dc.datasetSelect = function()
   dc.control_list_length = 0;
 
   // Collect the results for lists of methods.
-  var sqlstr = "SELECT DISTINCT methods.name, methods.parameters, libraries.name FROM methods, results, datasets, libraries WHERE results.dataset_id == datasets.id AND results.method_id == methods.id AND datasets.name == '" + dc.dataset_name + "' AND libraries.id == results.libary_id ORDER BY methods.name;";
-  dc.methods = db.exec(sqlstr);
+  var sqlstr = "SELECT DISTINCT methods.name as method, methods.parameters as parameter, libraries.name as lib FROM methods, results, datasets, libraries WHERE results.dataset_id = datasets.id AND results.method_id = methods.id AND datasets.name = '" + dc.dataset_name + "' AND libraries.id = results.libary_id ORDER BY methods.name;";
+  dc.methods = dbExec(sqlstr);
+  dc.methods = dbType === "sqlite" ? dc.methods[0].values : dc.methods;
 }
 
 // The user has requested to add a new thing.
@@ -110,7 +109,7 @@ dc.clickAddButton = function()
 
   // Add list of methods.
   var newbox = document.getElementById("method_select_" + String(dc.control_list_length - 1));
-  distinct_methods = dc.methods[0].values.map(function(d) { return d[0]; }).reduce(function(p, c) { if(p.indexOf(c) < 0) p.push(c); return p; }, []);
+  distinct_methods = dc.methods.map(function(d) { return dbType === "sqlite" ? d[0] : d.method; }).reduce(function(p, c) { if(p.indexOf(c) < 0) p.push(c); return p; }, []);
   for (i = 0; i < distinct_methods.length; i++)
   {
     var new_option = document.createElement("option");
@@ -126,7 +125,7 @@ dc.methodControlListSelect = function(id)
   var method_name = selectbox.options[selectbox.selectedIndex].text;
 
   // Now we need to add parameters.
-  distinct_parameters = dc.methods[0].values.map(function(d) { return d; }).reduce(function(p, c) { if(c[0] != method_name) { return p; } else if(p.indexOf(c[1]) < 0) { p.push(c[1]); } return p; }, []);
+  distinct_parameters = dc.methods.map(function(d) { return d; }).reduce(function(p, c) { if((dbType === "sqlite" ? c[0] : c.method) != method_name) { return p; } else if(p.indexOf((dbType === "sqlite" ? c[1] : c.parameter)) < 0) { p.push((dbType === "sqlite" ? c[1] : c.parameter)); } return p; }, []);
   var parambox = document.getElementById("param_select_" + id);
   clearSelectBox(parambox);
   for (i = 0; i < distinct_parameters.length; i++)
@@ -147,7 +146,7 @@ dc.paramControlListSelect = function(id)
   var param_name = param_select_box.options[param_select_box.selectedIndex].text;
   if (param_name == "[no parameters]") { param_name = ""; }
 
-  distinct_libraries = dc.methods[0].values.map(function(d) { return d; }).reduce(function(p, c) { if(c[0] != method_name || c[1] != param_name) { return p; } else if(p.indexOf(c[2]) < 0) { p.push(c[2]); } return p; }, []);
+  distinct_libraries = dc.methods.map(function(d) { return d; }).reduce(function(p, c) { if((dbType === "sqlite" ? c[0] : c.method) != method_name || (dbType === "sqlite" ? c[1] : c.parameter) != param_name) { return p; } else if(p.indexOf((dbType === "sqlite" ? c[2] : c.lib)) < 0) { p.push((dbType === "sqlite" ? c[2] : c.lib)); } return p; }, []);
 
   var library_select_box = document.getElementById("library_select_" + id);
   clearSelectBox(library_select_box);
@@ -182,7 +181,12 @@ dc.clickRedrawMethods = function()
     var librarybox = document.getElementById("library_select_" + String(i));
     var library_name = librarybox.options[librarybox.selectedIndex].text;
 
-    sqlstr = sqlstr + "SELECT DISTINCT results.time, results.var, methods.name, methods.parameters, libraries.name, " + String(i) + " AS sort FROM results, methods, libraries, datasets WHERE results.libary_id == libraries.id AND libraries.name == '" + library_name + "' AND results.dataset_id == datasets.id AND datasets.name == '" + dc.dataset_name + "' AND results.method_id == methods.id AND methods.name == '" + method_name + "' AND methods.parameters == '" + param_name + "' GROUP BY libraries.name";
+    sqlstr = sqlstr + "SELECT DISTINCT * FROM " +
+      "(SELECT results.time as time, results.var as var, methods.name as method, methods.parameters as parameter, libraries.name as lib, " + String(i) + " AS sort, results.build_id as bid " +
+      "FROM results, methods, libraries, datasets " +
+      "WHERE results.libary_id = libraries.id AND libraries.name = '" + library_name + "' AND results.dataset_id = datasets.id AND datasets.name = '" + dc.dataset_name + "' AND results.method_id = methods.id AND methods.name = '" + method_name + "' AND methods.parameters = '" + param_name + "' " +
+      "ORDER BY bid DESC " +
+      ") tmp GROUP BY lib";
 
     if (i < dc.control_list_length - 1)
     {
@@ -190,7 +194,8 @@ dc.clickRedrawMethods = function()
     }
   }
   sqlstr = sqlstr + " ORDER BY sort ASC;";
-  dc.results = db.exec(sqlstr);
+  dc.results = dbExec(sqlstr);
+  dc.results = dbType === "sqlite" ? dc.results[0].values : dc.results;
 
   dc.clearChart();
   dc.buildChart();
@@ -217,12 +222,26 @@ dc.clearChart = function()
 // Build the chart and display it on screen.
 dc.buildChart = function()
 {
+  dc.results = dc.results.map(function(d) {
+  var runtime = dbType === "sqlite" ? d[0] : d.time;
+  if (runtime == 0)
+  {
+    if (dbType === "sqlite")
+    {
+      d[0] = "failure";
+    }
+    else
+    {
+      d.time = "failure";
+    }
+  }
+  return d; })
+
   var input_range = [];
   for (i = 0; i < dc.control_list_length; i++) { input_range.push(i); }
 
   var bar_width = width / dc.control_list_length;
-
-  var max_runtime = d3.max(dc.results[0].values, function(d) { return mapRuntime(d[0], 0); });
+  var max_runtime = d3.max(dc.results, function(d) { return mapRuntime(dbType === "sqlite" ? d[0] : d.time, 0); });
   if (max_runtime == 0) { max_runtime = 0.01; }
 
   var y_scale = d3.scale.linear()
@@ -230,7 +249,7 @@ dc.buildChart = function()
       .range([height, 0]);
 
   // Set up axes.
-//  var xAxis = d3.svg.axis().scale(x_scale).orient("bottom");
+  //  var xAxis = d3.svg.axis().scale(x_scale).orient("bottom");
   var yAxis = d3.svg.axis().scale(y_scale).orient("left").tickFormat(d3.format(".2s"));
 
   // Create svg object.
@@ -256,22 +275,24 @@ dc.buildChart = function()
       .attr("class", "d3-tip")
       .offset([-10, 0])
       .html(function(d) {
-        var runtime = d[0];
-        if (d[0] != ">9000" && d[0] != "failure") { runtime = d[0].toFixed(1); }
-        return "<b>" + d[4] + "</b> implementation of<br><b>" + d[2] + "</b> with parameters<br><b>" + d[3] + "</b>: " + runtime + "s";
+        var runtime = dbType === "sqlite" ? d[0] : d.time;
+        var parameterValue = (dbType === "sqlite" ? d[3] : d.parameter);
+        parameterValue = parameterValue == "" ? "none" : parameterValue;
+        if (runtime != ">9000" && runtime != "failure") { runtime = parseFloat(runtime).toFixed(3); }
+        return "<b>" + (dbType === "sqlite" ? d[4] : d.lib) + "</b> implementation of<br><b>" + (dbType === "sqlite" ? d[2] : d.method) + "</b> with parameters<br><b>" + parameterValue + "</b>: " + runtime + "s";
       });
 
   // Add all of the data points.
   var bar = svg.selectAll("rect")
-      .data(dc.results[0].values)
+      .data(dc.results)
       .enter().append("g")
       .attr("transform", function(d, i) { return "translate(" + i * bar_width + ",0)"; });
 
   bar.append("rect")
-      .attr("y", function(d) { if (d[0] == "failure") { return y_scale(max_runtime); } else { return y_scale(mapRuntime(d[0], max_runtime)); } })
-      .attr("height", function(d) { if (d[0] == "failure") { return height - y_scale(max_runtime); } else { return Math.max(height - y_scale(mapRuntime(d[0], max_runtime)), 1); } })
+      .attr("y", function(d) { if ((dbType === "sqlite" ? d[0] : d.time) == "failure") { return y_scale(max_runtime); } else { return y_scale(mapRuntime(dbType === "sqlite" ? d[0] : d.time, max_runtime)); } })
+      .attr("height", function(d) { if (d[0] == "failure") { return height - y_scale(max_runtime); } else { return Math.max(height - y_scale(mapRuntime(dbType === "sqlite" ? d[0] : d.time, max_runtime)), 1); } })
       .attr("width", bar_width - 1)
-      .attr("fill", function(d) { if (d[0] == "failure") { return "firebrick"; } else { return "steelblue"; } })
+      .attr("fill", function(d) { if ((dbType === "sqlite" ? d[0] : d.time) == "failure") { return "firebrick"; } else { return "steelblue"; } })
       .on("mouseover", tip.show)
       .on("mouseout", tip.hide);
 
@@ -280,6 +301,6 @@ dc.buildChart = function()
       .attr("y", function(d) { return height + 4; })
       .attr("dy", ".75em")
       .text(function(d, i) { return i; });
- 
+
   svg.call(tip);
 }
