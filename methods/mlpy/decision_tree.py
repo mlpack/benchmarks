f8468be832fb42bf77eaba72efc4078ec4a09360
Dewang Sultania <dewangsultania@gmail.com>
2017-03-18 19:33:17 +0530

    added benchmark code for Decision Tree in mlpy

---
+++ methods/mlpy/decision_tree.py
@@ -0,0 +1,136 @@
+import os
+import sys
+import inspect
+
+# Import the util path, this method even works if the path contains symlinks to
+# modules.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../util")))
+if cmd_subfolder not in sys.path:
+  sys.path.insert(0, cmd_subfolder)
+
+#Import the metrics definitions path.
+metrics_folder = os.path.realpath(os.path.abspath(os.path.join(
+  os.path.split(inspect.getfile(inspect.currentframe()))[0], "../metrics")))
+if metrics_folder not in sys.path:
+  sys.path.insert(0, metrics_folder)
+
+from log import *
+from timer import *
+from definitions import *
+from misc import *
+
+import numpy as np
+import mlpy
+
+'''
+This class implements the Decision Tree Classifier benchmark.
+'''
+class DECISIONTREE(object):
+
+  '''
+  Create the Decision Tree Classifier benchmark instance.
+  @param dataset - Input dataset to perform Decision Tree Classification on.
+  @param timeout - The time until the timeout. Default no timeout.
+  @param verbose - Display informational messages.
+  '''
+  def __init__(self, dataset, timeout=0, verbose=True):
+    self.verbose = verbose
+    self.dataset = dataset
+    self.timeout = timeout
+    self.model = None
+    self.stumps = 0
+    self.minsize = 1
+
+  '''
+  Build the model for the DecisionTree Classifier.
+  @param data - The train data.
+  @param labels - The labels for the train set.
+  @return The created model.
+  '''
+  def BuildModel(self, data, labels):
+    # Create and train the classifier.
+    DC = mlpy.ClassTree(stumps=self.stumps, minsize=self.minsize)
+    DC.learn(data, labels)
+    return DC
+
+  '''
+  Use the mlpy libary to implement the Decision Tree Classifier.
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not
+  successful.
+  '''
+  def DCMlpy(self, options):
+    def RunDCMlpy(q):
+      totalTimer = Timer()
+
+      Log.Info("Loading dataset", self.verbose)
+      trainData, labels = SplitTrainData(self.dataset)
+      testData = LoadDataset(self.dataset[1])
+
+      # Get all the parameters.
+      s = re.search("--stumps (\d+)", options)
+      m = re.search("-m (\d+)", options)
+
+      self.stumps = 0 if not s else int(s.group(1))
+      self.minsize = 1 if not m else int(m.group(1))
+
+      try:
+        with totalTimer:
+          self.model = self.BuildModel(trainData, labels)
+          # Run Decision Tree Classifier on the test dataset.
+          self.model.pred(testData)
+      except Exception as e:
+        Log.Debug(str(e))
+        q.put(-1)
+        return -1
+
+      time = totalTimer.ElapsedTime()
+      q.put(time)
+
+      return time
+
+    return timeout(RunDCMlpy, self.timeout)
+
+  '''
+  Perform the Decision Tree Classifier. If the method has been
+  successfully completed return the elapsed time in seconds.
+  @param options - Extra options for the method.
+  @return - Elapsed time in seconds or a negative value if the method was not
+  successful.
+  '''
+  def RunMetrics(self, options):
+    Log.Info("Perform Classification using Tree.", self.verbose)
+
+    results = None
+    if len(self.dataset) >= 2:
+      results = self.DCMlpy(options)
+
+      if results < 0:
+        return results
+    else:
+      Log.Fatal("This method requires two datasets.")
+
+    # Datastructure to store the results.
+    metrics = {'Runtime' : results}
+
+    if len(self.dataset) >= 3:
+
+      # Check if we need to create a model.
+      if not self.model:
+        trainData, labels = SplitTrainData(self.dataset)
+        self.model = self.BuildModel(trainData, labels)
+
+      testData = LoadDataset(self.dataset[1])
+      truelabels = LoadDataset(self.dataset[2])
+
+      predictedlabels = self.model.pred(testData)
+
+      confusionMatrix = Metrics.ConfusionMatrix(truelabels, predictedlabels)
+      metrics['ACC'] = Metrics.AverageAccuracy(confusionMatrix)
+      metrics['MCC'] = Metrics.MCCMultiClass(confusionMatrix)
+      metrics['Precision'] = Metrics.AvgPrecision(confusionMatrix)
+      metrics['Recall'] = Metrics.AvgRecall(confusionMatrix)
+      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, predictedlabels)
+
+    return metrics
