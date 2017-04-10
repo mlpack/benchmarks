a1d80e08a0f01123cbb3471d677057acb49d5f25
Ryan Curtin <ryan@ratml.org>
2017-04-10 15:42:33 -0400

    Fix install path and get around setuptools warning.

--- libraries/annoy_install.sh
+++ libraries/annoy_install.sh
@@ -26,4 +26,5 @@ tar -xzpf annoy*.tar.gz --strip-components=1 -C annoy/
 
 cd annoy/
 python3 setup.py build
-python3 setup.py install --prefix=../ -O2
+PYVER=`python3 -c 'import sys; print("python" + sys.version[0:3])'`;
+PYTHONPATH=../lib/$PYVER/site-packages/ python3 setup.py install --prefix=../ -O2
