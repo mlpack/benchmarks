59d05dec89755e2b3afc29ca263759be5a8b1ef3
Ryan Curtin <ryan@ratml.org>
2017-05-11 15:48:07 -0400

    Remove HLearn install and configuration; doesn't work with ghc 8.

--- libraries/hlearn_install.sh
+++ libraries/hlearn_install.sh
@@ -8,33 +8,36 @@
 # One HLearn.tar.gz file should be located in this directory.  Also since
 # HLearn depends on subhask, a subhask*.tar.gz file should also be located in
 # this directory.
-tars=`ls HLearn.tar.gz | wc -l`;
-if [ "$tars" -eq "0" ];
-then
-  echo "No source HLearn.tar.gz found in libraries/!"
-  exit 1
-fi
+#
+# HLearn doesn't work with ghc 8, so this installation is commented out.
 
-subhasktars=`ls subhask.tar.gz | wc -l`;
-if [ "$tars" -eq "0" ];
-then
-  echo "No source subhask.tar.gz found in libraries/!"
-  exit 1
-fi
+#tars=`ls HLearn.tar.gz | wc -l`;
+#if [ "$tars" -eq "0" ];
+#then
+#  echo "No source HLearn.tar.gz found in libraries/!"
+#  exit 1
+#fi
+
+#subhasktars=`ls subhask.tar.gz | wc -l`;
+#if [ "$tars" -eq "0" ];
+#then
+#  echo "No source subhask.tar.gz found in libraries/!"
+#  exit 1
+#fi
 
 # Remove any old directory.
-rm -rf hlearn/
-mkdir hlearn/
-tar -xzpf HLearn.tar.gz --strip-components=1 -C hlearn/
+#rm -rf hlearn/
+#mkdir hlearn/
+#tar -xzpf HLearn.tar.gz --strip-components=1 -C hlearn/
 
-rm -rf subhask/
-mkdir subhask/
-tar -xvzpf subhask.tar.gz --strip-components=1 -C subhask/
+#rm -rf subhask/
+#mkdir subhask/
+#tar -xvzpf subhask.tar.gz --strip-components=1 -C subhask/
 
-cd hlearn/
-cabal sandbox init
-cabal sandbox add-source ../subhask
-cabal update
-cabal install --only-dependencies --force-reinstalls
-cabal build
-cabal install
+#cd hlearn/
+#cabal sandbox init
+#cabal sandbox add-source ../subhask
+#cabal update
+#cabal install --only-dependencies --force-reinstalls
+#cabal build
+#cabal install
