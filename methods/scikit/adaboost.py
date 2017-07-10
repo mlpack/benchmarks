61ea02e4e664d8aba948838e03914168149c5da5
Dewang Sultania <dewangsultania@gmail.com>
2017-07-10 22:07:34 +0530

    Avoid building the model twice

--- methods/scikit/adaboost.py
+++ methods/scikit/adaboost.py
@@ -47,6 +47,7 @@ class ADABOOST(object):
     self.dataset = dataset
     self.timeout = timeout
     self.model = None
+    self.predictions = None
     self.build_opts = {}
 
   '''
@@ -96,18 +97,23 @@ class ADABOOST(object):
         with totalTimer:
           self.model = self.BuildModel(trainData, labels)
           # Run AdaBoost classifier on the test dataset.
-          self.model.predict(testData)
+          self.predictions = self.model.predict(testData)
       except Exception as e:
         Log.Debug(str(e))
         q.put(-1)
         return -1
 
       time = totalTimer.ElapsedTime()
-      q.put(time)
+      q.put((time, self.predictions))
 
       return time
 
-    return timeout(RunADABOOSTScikit, self.timeout)
+    result = timeout(RunADABOOSTScikit, self.timeout)
+    # Check for error, in this case the tuple doesn't contain extra information.
+    if len(result) > 1:
+       self.predictions = result[1]
+
+    return result[0]
 
   '''
   Perform the AdaBoost classifier. If the method has been
@@ -134,20 +140,13 @@ class ADABOOST(object):
 
     if len(self.dataset) >= 3:
 
-      # Check if we need to create a model.
-      if not self.model:
-        trainData, labels = SplitTrainData(self.dataset)
-        self.model = self.BuildModel(trainData, labels)
-
-      testData = LoadDataset(self.dataset[1])
       truelabels = LoadDataset(self.dataset[2])
-      predictedlabels = self.model.predict(testData)
 
-      confusionMatrix = Metrics.ConfusionMatrix(truelabels, predictedlabels)
+      confusionMatrix = Metrics.ConfusionMatrix(truelabels, self.predictions)
       metrics['ACC'] = Metrics.AverageAccuracy(confusionMatrix)
       metrics['MCC'] = Metrics.MCCMultiClass(confusionMatrix)
       metrics['Precision'] = Metrics.AvgPrecision(confusionMatrix)
       metrics['Recall'] = Metrics.AvgRecall(confusionMatrix)
-      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, predictedlabels)
+      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, self.predictions)
 
     return metrics
