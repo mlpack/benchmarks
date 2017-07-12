c141a37ae3a8b6c993e538ca9c93aad1f435d17d
Dewang Sultania <dewangsultania@gmail.com>
2017-07-12 20:42:07 +0530

    Avoiding failing in case no test set is provided.

--- methods/scikit/svm.py
+++ methods/scikit/svm.py
@@ -109,7 +109,10 @@ class SVM(object):
         return -1
 
       time = totalTimer.ElapsedTime()
-      q.put((time, self.predictions))
+      if len(self.dataset) > 1:
+        q.put((time, self.predictions))
+      else:
+        q.put(time)
 
       return time
 
@@ -117,8 +120,9 @@ class SVM(object):
     # Check for error, in this case the tuple doesn't contain extra information.
     if len(result) > 1:
        self.predictions = result[1]
-
-    return result[0]
+       return result[0]
+    
+    return result
 
   '''
   Perform the Support vector machines. If the method has been
