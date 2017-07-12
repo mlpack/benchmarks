016710a1e47d7a4c815bac4586b1aaf67fe25841
Dewang Sultania <dewangsultania@gmail.com>
2017-07-12 21:59:17 +0530

    Avoid failing in case test file is not passed.

--- methods/scikit/nbc.py
+++ methods/scikit/nbc.py
@@ -92,7 +92,10 @@ class NBC(object):
         return -1
 
       time = totalTimer.ElapsedTime()
-      q.put((time, self.predictions))
+      if len(self.dataset) > 1:
+        q.put((time, self.predictions))
+      else:
+        q.put(time)
 
       return time
 
@@ -100,8 +103,9 @@ class NBC(object):
     # Check for error, in this case the tuple doesn't contain extra information.
     if len(result) > 1:
        self.predictions = result[1]
+       return result[0]
 
-    return result[0]
+    return result
   '''
   Perform Naive Bayes Classifier. If the method has been successfully
   completed return the elapsed time in seconds.
