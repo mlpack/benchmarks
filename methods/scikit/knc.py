b11160439b9693844e85f7672691a461ca9f3f4b
Dewang Sultania <dewangsultania@gmail.com>
2017-07-12 21:54:43 +0530

    Avoid failing in case test is not passed.

--- methods/scikit/knc.py
+++ methods/scikit/knc.py
@@ -104,7 +104,10 @@ class KNC(object):
         return -1
 
       time = totalTimer.ElapsedTime()
-      q.put((time, self.predictions))
+      if len(self.dataset) > 1:
+        q.put((time, self.predictions))
+      else:
+        q.put(time)
 
       return time
 
@@ -112,8 +115,9 @@ class KNC(object):
     # Check for error, in this case the tuple doesn't contain extra information.
     if len(result) > 1:
        self.predictions = result[1]
+       return result[0]
 
-    return result[0]
+    return result
 
   '''
   Perform the k-nearest neighbors Classifier. If the method has been
