3806e3ff2917369258f894c27b9337ba8681cdef
Dewang Sultania <dewangsultania@gmail.com>
2017-07-12 20:48:33 +0530

    Avoid failing in case test set is not passed.

--- methods/shogun/nbc.py
+++ methods/shogun/nbc.py
@@ -93,7 +93,10 @@ class NBC(object):
         return -1
 
       time = totalTimer.ElapsedTime()
-      q.put((time, self.predictions))
+      if len(self.dataset) > 1:
+        q.put((time, self.predictions))
+      else:
+        q.put(time)
 
       return time
 
@@ -101,8 +104,9 @@ class NBC(object):
     # Check for error, in this case the tuple doesn't contain extra information.
     if len(result) > 1:
        self.predictions = result[1]
+       return result[0]
 
-    return result[0]
+    return result
 
   '''
   Perform Naive Bayes Classifier. If the method has been successfully completed
