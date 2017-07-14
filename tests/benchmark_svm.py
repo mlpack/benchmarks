bdbff82698143cd21298a1ba51ae80be0405d646
Dewang Sultania <dewangsultania@gmail.com>
2017-07-15 01:32:05 +0530

    Include shogun and matlab tests for SVC

--- tests/benchmark_svm.py
+++ tests/benchmark_svm.py
@@ -1,7 +1,6 @@
 '''
   @file benchmark_svm.py
-  
-  Test for the Parametric Support Vector Classifier scripts.
+  Test for the svm scripts.
 '''
 
 import unittest
@@ -16,6 +15,42 @@ if cmd_subfolder not in sys.path:
   sys.path.insert(0, cmd_subfolder)
 
 from loader import *
+
+'''
+Test the scikit svm script.
+'''
+
+class SVM_SCIKIT_TEST(unittest.TestCase):
+
+  '''
+  Test initialization.
+  '''
+  def setUp(self):
+    self.dataset = ['datasets/iris_train.csv','datasets/iris_test.csv','datasets/iris_labels.csv']
+    self.verbose = False
+    self.timeout = 9000
+
+    module = Loader.ImportModuleFromPath("methods/scikit/svm.py")
+    obj = getattr(module, "SVM")
+    self.instance = obj(self.dataset, verbose=self.verbose, timeout=self.timeout)
+
+  '''
+  Test the constructor.
+  '''
+  def test_Constructor(self):
+    self.assertEqual(self.instance.verbose, self.verbose)
+    self.assertEqual(self.instance.timeout, self.timeout)
+    self.assertEqual(self.instance.dataset, self.dataset)
+
+  '''
+  Test the 'RunMetrics' function.
+  '''
+  def test_RunMetrics(self):
+    result = self.instance.RunMetrics({})
+    self.assertTrue(result["Runtime"] > 0)
+    self.assertTrue(result["ACC"] > 0)
+    self.assertTrue(result["Precision"] > 0)
+    self.assertTrue(result["Recall"] > 0)
 
 '''
 Test the shogun Parametric Support Vector Classifier script.
@@ -52,43 +87,6 @@ class SVC_SHOGUN_TEST(unittest.TestCase):
     self.assertTrue(result["Precision"] > 0)
     self.assertTrue(result["Recall"] > 0)
 
-
-'''
-Test the Scikit Parametric Support Vector Classifier script.
-'''
-class SVC_SCIKIT_TEST(unittest.TestCase):
-
-  '''
-  Test initialization.
-  '''
-  def setUp(self):
-    self.dataset = ['datasets/iris_train.csv', 'datasets/iris_test.csv','datasets/iris_labels.csv']
-    self.verbose = False
-    self.timeout = 240
-
-    module = Loader.ImportModuleFromPath("methods/scikit/svm.py")
-    obj = getattr(module, "SVM")
-    self.instance = obj(self.dataset, verbose=self.verbose, timeout=self.timeout)
-
-  '''
-  Test the constructor.
-  '''
-  def test_Constructor(self):
-    self.assertEqual(self.instance.verbose, self.verbose)
-    self.assertEqual(self.instance.timeout, self.timeout)
-    self.assertEqual(self.instance.dataset, self.dataset)
-
-  '''
-  Test the 'RunMetrics' function.
-  '''
-  def test_RunMetrics(self):
-    result = self.instance.RunMetrics({})
-    self.assertTrue(result["Runtime"] > 0)
-    self.assertTrue(result["ACC"] > 0)
-    self.assertTrue(result["Precision"] > 0)
-    self.assertTrue(result["Recall"] > 0)
-
-    
 
 '''
 Test the matlab Parametric Support Vector Classifier script.
