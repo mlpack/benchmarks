e2098c936c92eed262310c7e00d8f41fc99a4cc7
Dewang Sultania <dewangsultania@gmail.com>
2017-07-14 15:16:15 +0530

    Avoid building the model twice.

--- methods/scikit/svm.py
+++ methods/scikit/svm.py
@@ -47,6 +47,7 @@ class SVM(object):
     self.dataset = dataset
     self.timeout = timeout
     self.model = None
+    self.predictions = None
     self.opts = {}
 
   '''
@@ -102,18 +103,26 @@ class SVM(object):
         with totalTimer:
           self.model = self.BuildModel(trainData, labels)
           # Run Support vector machines on the test dataset.
-          self.model.predict(testData)
+          self.predictions = self.model.predict(testData)
       except Exception as e:
         Log.Debug(str(e))
         q.put(-1)
         return -1
 
       time = totalTimer.ElapsedTime()
-      q.put(time)
+      if len(self.dataset) > 1:
+        q.put((time, self.predictions))
+      else:
+        q.put(time)
 
       return time
 
-    return timeout(RunSVMScikit, self.timeout)
+    result = timeout(RunSVMScikit, self.timeout)
+    if len(result) > 1:
+      self.predictions = result[1]
+      return result[0]
+    
+    return result
 
   '''
   Perform the Support vector machines. If the method has been
@@ -140,21 +149,14 @@ class SVM(object):
 
     if len(self.dataset) >= 3:
 
-      # Check if we need to create a model.
-      if not self.model:
-        trainData, labels = SplitTrainData(self.dataset)
-        self.model = self.BuildModel(trainData, labels)
-
-      testData = LoadDataset(self.dataset[1])
       truelabels = LoadDataset(self.dataset[2])
-      predictedlabels = self.model.predict(testData)
 
-      confusionMatrix = Metrics.ConfusionMatrix(truelabels, predictedlabels)
+      confusionMatrix = Metrics.ConfusionMatrix(truelabels, self.predictions)
       metrics['ACC'] = Metrics.AverageAccuracy(confusionMatrix)
       metrics['MCC'] = Metrics.MCCMultiClass(confusionMatrix)
       metrics['Precision'] = Metrics.AvgPrecision(confusionMatrix)
       metrics['Recall'] = Metrics.AvgRecall(confusionMatrix)
-      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, predictedlabels)
+      metrics['MSE'] = Metrics.SimpleMeanSquaredError(truelabels, self.predictions)
 
     return metrics
 
