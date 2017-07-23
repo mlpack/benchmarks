370f026fefc69d92d1adae95ba3fc15e25454bd6
Dewang Sultania <dewangsultania@gmail.com>
2017-07-23 22:00:56 +0530

    Update comment and use a single return statement.

--- methods/scikit/dtc.py
+++ methods/scikit/dtc.py
@@ -120,10 +120,9 @@ class DTC(object):
       return time
 
     result = timeout(RunDTCScikit, self.timeout)
-    # Check for error, in this case the tuple doesn't contain extra information.
+    # Check for error, in this case the list doesn't contain extra information.
     if len(result) > 1:
        self.predictions = result[1]
-       return result[0]
 
     return result[0]
 
