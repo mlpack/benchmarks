fa450e5e990d11eb03291676373c6ad3a9ee9d9a
Dewang Sultania <dewangsultania@gmail.com>
2017-07-23 22:01:45 +0530

    Update comment and use a single return statement.

--- methods/scikit/knc.py
+++ methods/scikit/knc.py
@@ -112,10 +112,9 @@ class KNC(object):
       return time
 
     result = timeout(RunKNCScikit, self.timeout)
-    # Check for error, in this case the tuple doesn't contain extra information.
+    # Check for error, in this case the list doesn't contain extra information.
     if len(result) > 1:
        self.predictions = result[1]
-       return result[0]
 
     return result[0]
 
