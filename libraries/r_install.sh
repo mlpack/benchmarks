d7e0341aaaa42f7ded375943eec5ded2d564e430
Dewang Sultania <dewangsultania@gmail.com>
2017-08-02 05:40:51 +0530

    Install files for R

---
+++ libraries/r_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Wrapper script to unpack and build R.
+#
+# Include files will be installed to ../include/.
+# Library files will be installed to ../lib/.
+#
+# One R.tar.gz file should be located in this directory.
+tars=`ls R.tar.gz | wc -l`;
+if [ "$tars" -eq "0" ];
+then
+  echo "No source R.tar.gz found in libraries/!"
+  exit 1
+fi
+
+# Remove any old directory.
+rm -rf R/
+mkdir R/
+tar -xzpf R.tar.gz --strip-components=1 -C R/
+
+cd R/
+./configure --prefix=/opt/R/3.4.1 --enable-R-shlib
+make
+make install
