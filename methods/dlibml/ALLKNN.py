483dfa492a3a738076ecd946a59c248ab2389990
Marcus Edel <marcus.edel@fu-berlin.de>
2019-03-09 22:33:26 +0100

    Update dlibml scripts.

--- methods/dlibml/ALLKNN.py
+++ methods/dlibml/ALLKNN.py
@@ -1,11 +1,9 @@
 '''
-  @file ALLKNN.py
+  @file allknn.py
   Class to benchmark the ALLKNN method with dlibml.
 '''
 
-import os
-import sys
-import inspect
+import os, sys, inspect, shlex, subprocess
 
 # Import the util path, this method even works if the path contains symlinks to
 # modules.
@@ -14,102 +12,43 @@ cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(
 if cmd_subfolder not in sys.path:
   sys.path.insert(0, cmd_subfolder)
 
-from log import *
-from profiler import *
-
-import shlex
-import subprocess
-import re
-import collections
+from util import *
 
 '''
 This class implements the ALLKNN benchmark.
 '''
-class ALLKNN(object):
+class DLIBML_ALLKNN(object):
+  def __init__(self, method_param, run_param):
+    # Assemble run command.
+    self.cmd = run_param["dlibml_path"] + "dlibml_allknn"
+    if "datasets" in method_param:
+      dataset = check_dataset(method_param["datasets"], ["csv", "txt"])
+      if len(dataset) == 2:
+        self.cmd += " -r " + dataset[0] + " -q " + dataset[1]
+      elif len(dataset) == 1:
+        self.cmd += " -r " + dataset[0]
+    if "k" in method_param:
+      self.cmd += " -k " + str(method_param["k"])
+    self.cmd += " -v"
 
-  '''
-  Create the ALLKNN benchmark instance, show some informations
-  and return the instance.
-  @param dataset - Input dataset to perform ALLKNN on.
-  @param timeout - The time until the timeout. Default no timeout.
-  @param path - Path to the flann executable.
-  @param verbose - Display informational messages.
-  '''
-  def __init__(self, dataset, timeout=0, path=os.environ["DLIBML_PATH"],
-        verbose = True):
-    self.verbose = verbose
-    self.dataset = dataset
-    self.path = path
-    self.timeout = timeout
+    self.info = "DLIBML_ALLKNN (" + self.cmd + ")"
+    self.timeout = run_param["timeout"]
 
+  def __str__(self):
+    return self.info
 
-  '''
-  Perform ALLKNN. If the method has been successfully completed
-  return the elapsed time in seconds.
-  @param options - Extra options for the method.
-  @return - Elapsed time in seconds or a negative value if the method was not
-  successful.
-  '''
-  def RunMetrics(self, options):
-    Log.Info("Perform ALLKNN.", self.verbose)
-
-    # Parse options into string.
-    optionsStr = ""
-    if "k" in options:
-      optionsStr = "-k " + str(options.pop("k"))
-    else:
-      Log.Fatal("Required parameter 'k' is missing!")
-      raise Exception("missing parameter")
-
-    if len(options) > 0:
-      Log.Fatal("Unknown parameters: " + str(options))
-      raise Exception("unknown parameters")
-
-    cmd = shlex.split(self.path + "dlibml_allknn -r " + self.dataset +
-          " -v " + optionsStr)
-
-    # Run command with the nessecary arguments and return its output as a byte
-    # string. We have untrusted input so we disable all shell based features.
+  def metric(self):
     try:
-      s = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=False,
-          timeout=self.timeout)
+      output = subprocess.check_output(self.cmd, stderr=subprocess.STDOUT,
+        shell=True, timeout=self.timeout)
     except subprocess.TimeoutExpired as e:
-      Log.Warn(str(e))
-      return -2
+      raise Exception("method timeout")
     except Exception as e:
-      Log.Fatal("Could not execute command: " + str(cmd))
-      return -1
+      raise Exception(str(e))
 
-    # Datastructure to store the results.
-    metrics = {}
+    metric = {}
+    timer = parse_timer(output)
+    if timer:
+      metric["runtime"] = timer["Nearest_Neighbors"]
 
-    # Parse data: runtime.
-    timer = self.parseTimer(s)
-
-    if timer != -1:
-      metrics['Runtime'] = timer.Nearest_Neighbors
-
-      Log.Info(("total time: %fs" % (metrics['Runtime'])), self.verbose)
-
-    return metrics
-
-  '''
-  Parse the timer data form a given string.
-  @param data - String to parse timer data from.
-  @return - Namedtuple that contains the timer data or -1 in case of an error.
-  '''
-  def parseTimer(self, data):
-    # Compile the regular expression pattern into a regular expression object to
-    # parse the timer data.
-    pattern = re.compile(r"""
-        .*?Nearest_Neighbors: (?P<Nearest_Neighbors>.*?)s.*?
-        """, re.VERBOSE|re.MULTILINE|re.DOTALL)
-
-    match = pattern.match(data.decode())
-    if not match:
-      Log.Fatal("Can't parse the data: wrong format")
-      return -1
-    else:
-      # Create a namedtuple and return the timer data.
-      timer = collections.namedtuple("timer", ["Nearest_Neighbors"])
-      return timer(float(match.group("Nearest_Neighbors")))	
+    return metric
