1ba5877457bd5818f949e11a8c10c522eb678a32
braceletboy <rukman.sai2@gmail.com>
2019-04-25 00:50:39 +0530

    Update Shogun's Kernel PCA benchmark
    The benchmark now has more configurable options and resolves issue #136
    	modified:   methods/shogun/kernel_pca.py

--- methods/shogun/kernel_pca.py
+++ methods/shogun/kernel_pca.py
@@ -1,6 +1,7 @@
 '''
   @file kernel_pca.py
   @author Marcus Edel
+  @contributor Rukmangadh Sai Myana
 
   Kernel Principal Components Analysis with shogun.
 '''
@@ -16,49 +17,208 @@ if cmd_subfolder not in sys.path:
 
 from util import *
 from shogun import RealFeatures, KernelPCA
-from shogun import GaussianKernel, PolyKernel, LinearKernel, SigmoidKernel
+from shogun import (
+  GaussianKernel,
+  PolyKernel,
+  LinearKernel,
+  SigmoidKernel,
+  BesselKernel,
+  PowerKernel,
+  LogKernel,
+  CauchyKernel,
+  ConstKernel,
+  DiagKernel
+  )
+
+from shogun import (
+  EuclideanDistance,
+  BrayCurtisDistance,
+  ChiSquareDistance,
+  MahalanobisDistance,
+  CosineDistance,
+  TanimotoDistance,
+  MinkowskiMetric,
+  ManhattanMetric,
+  JensenMetric,
+  ChebyshewMetric,
+  CanberraMetric,
+  GeodesicMetric
+  )
 
 '''
 This class implements the Kernel Principal Components Analysis benchmark.
+
+Notes
+-----
+The following are the configurable options available for this benchmark:
+* target-dimensions: The new dimensions/dimensionality of samples in dataset.
+* kernel: "Gaussian", "Polynomial", "Linear", "Sigmoid", "Bessel", "Power",
+"Log", "Cauchy", "Constant", "Diagonal"
+
+Each kernel might have it's own seperate set of additional options. Check the
+Shogun Library's documentation for these. Most of these additional options are
+configurable for this benchmark.
 '''
 class SHOGUN_KPCA(object):
+  '''
+  Create the Kernel Principal Component Analysis benchmark instance.
+
+  @type method_param - dict
+  @param method_param - Extra options for the benchmarking method.
+  @type run_param - dict
+  @param run_param - Path option for executing the benchmark. Not used for
+  Shogun.
+  '''
   def __init__(self, method_param, run_param):
     self.info = "SHOGUN_KPCA ("  + str(method_param) +  ")"
 
     # Assemble run model parameter.
     self.data = load_dataset(method_param["datasets"], ["csv"])
-    self.method_param = method_param
 
+    self.kernel = "Gaussian"
+    if "kernel" in method_param:
+      self.kernel = str(method_param["kernel"])
+
+    self.d = self.data[0].shape[1]
+    if "target-dimensions" in method_param:
+      self.d = int(method_param["target-dimensions"])
+
+    self.degree = 3
+    if "degree" in method_param:
+      self.degree = int(method_param["degree"])
+
+    self.gamma = 2.0
+    if "gamma" in method_param:
+      self.gamma = float(method_param["gamma"])
+
+    self.distance = "Euclidean"
+    if "distance" in method_param:
+      self.distance = str(method_param["distance"])
+
+    self.cache_size = 10
+    if "cache-size" in method_param:
+      cache_size = int(method_param["cache-size"])
+
+    self.coef0 = 1.0
+    if "coef0" in method_param:
+      self.coef0 = float(method_param["coef0"])
+
+    self.order = 2.0
+    if "order" in method_param:
+      self.order = float(method_param["order"])
+
+    self.width = 2.0
+    if "width" in method_param:
+      self.width = float(method_param["width"])
+
+    self.sigma = 1.5
+    if "sigma" in method_param:
+      self.sigma = float(method_param["sigma"])
+
+    self.const = 2.0
+    if "constant" in method_param:
+      const = float(method_param["constant"])
+
+  '''
+  Return information about the benchmarking instance.
+
+  @rtype - str
+  @returns - Information as a single string
+  '''
   def __str__(self):
     return self.info
 
-  def metric(self):
-    data_feat = RealFeatures(self.data[0].T)
+  '''
+  Calculate metrics to be used for benchmarking.
 
+  @rtype - dict
+  @returns - Evaluated metrics.
+  '''
+  def metric(self):
     totalTimer = Timer()
     with totalTimer:
-      d = self.data[0].shape[1]
-      if "new_dimensionality" in self.method_param:
-        d = int(self.method_param["new_dimensionality"])
+      data_feat = RealFeatures(self.data[0].T)
 
-      # Get the kernel type and make sure it is valid.
-      if "kernel" in self.method_param:
-        kernel = str(self.method_param["kernel"])
+      if self.distance == "Euclidean":
+        distanceMethod = EuclideanDistance()
 
-      if "degree" in self.method_param:
-        degree = int(self.method_param["degree"])
+      elif self.distance == "Bray-Curtis":
+        distanceMethod = BrayCurtisDistance()
 
-      if kernel == "polynomial":
-        kernel = PolyKernel(data_feat, data_feat, degree, True)
-      elif kernel == "gaussian":
-        kernel = GaussianKernel(data_feat, data_feat, 2.0)
-      elif kernel == "linear":
-        kernel = LinearKernel(data_feat, data_feat)
-      elif kernel == "hyptan":
-        kernel = SigmoidKernel(data_feat, data_feat, 2, 1.0, 1.0)
+      elif self.distance == "Chi-Square":
+        distanceMethod = ChiSquareDistance()
 
-      model = KernelPCA(kernel)
-      model.set_target_dim(d)
+      elif self.distance == "MahalanobisDistance":
+        distanceMethod = MahalanobisDistance()
+
+      elif self.distance == "Cosine":
+        distanceMethod = CosineDistance()
+
+      elif self.distance == "Tanimoto":
+        distanceMethod = TanimotoDistance()
+
+      elif self.distance == "Minkowski":
+        distanceMethod = MinkowskiMetric()
+
+      elif self.distance == "Manhattan":
+        distanceMethod = ManhattanMetric()
+
+      elif self.distance == "Jensen":
+        distanceMethod = JensenMetric()
+
+      elif self.distance == "Chebyshev":
+        distanceMethod = ChebyshewMetric()
+
+      elif self.distance == "Canberra":
+        distanceMethod = CanberraMetric()
+
+      elif self.distance == "Geodesic":
+        distanceMethod = GeodesicMetric()
+
+      else:
+        raise ValueError("Provided distance is not supported by benchmark")
+
+      if self.kernel == "Polynomial":
+        kernelMethod = PolyKernel(data_feat, data_feat, self.degree, True,
+            self.cache_size)
+
+      elif self.kernel == "Gaussian":
+        kernelMethod = GaussianKernel(data_feat, data_feat, self.width,
+            self.cache_size)
+
+      elif self.kernel == "Linear":
+        kernelMethod = LinearKernel(data_feat, data_feat)
+
+      elif self.kernel == "Hyptan" or self.kernel == "Sigmoid":
+        kernelMethod = SigmoidKernel(data_feat, data_feat, self.cache_size,
+            self.gamma, self.coef0)
+
+      elif self.kernel == "Bessel":
+        kernelMethod = BesselKernel(data_feat, data_feat, self.order,
+          self.width, self.degree, distanceMethod, self.cache_size)
+
+      elif self.kernel == "Power":
+        kernelMethod = PowerKernel(data_feat, data_feat, self.degree,
+          distanceMethod)
+
+      elif self.kernel == "Log":
+        kernelMethod = LogKernel(data_feat, data_feat, self.degree,
+            distanceMethod)
+
+      elif self.kernel == "Cauchy":
+        kernelMethod = CauchyKernel(data_feat, data_feat, self.sigma,
+            distanceMethod)
+
+      elif self.kernel == "Constant":
+        kernelMethod = ConstKernel(data_feat, data_feat, self.const)
+
+      elif self.kernel == "Diagonal":
+        kernelMethod = DiagKernel(data_feat, data_feat, self.const)
+      else:
+        raise ValueError("Provided Kernel not supported by current benchmark")
+
+      model = KernelPCA(kernelMethod)
+      model.set_target_dim(self.d)
       model.init(data_feat)
       model.apply_to_feature_matrix(data_feat)
 
