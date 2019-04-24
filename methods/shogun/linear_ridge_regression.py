b6ff9acbd423487c81ee08839b99727cf58e0fcd
braceletboy <rukman.sai2@gmail.com>
2019-04-25 01:23:17 +0530

    Update Shogun's linear ridge regression benchmark.
    	modified:   methods/shogun/linear_ridge_regression.py

--- methods/shogun/linear_ridge_regression.py
+++ methods/shogun/linear_ridge_regression.py
@@ -1,6 +1,7 @@
 '''
   @file linear_ridge_regression.py
   @author Ali Mohamed
+  @contributor Rukmangadh Sai Myana
 
   Linear Ridge Regression with shogun.
 '''
@@ -17,11 +18,37 @@ if cmd_subfolder not in sys.path:
 from util import *
 from shogun import RegressionLabels, RealFeatures
 from shogun import LinearRidgeRegression as LRR
+from shogun import (
+  ST_AUTO,
+  ST_CPLEX,
+  ST_GLPK,
+  ST_NEWTON,
+  ST_DIRECT,
+  ST_ELASTICNET,
+  ST_BLOCK_NORM
+  )
 
 '''
-This class implements the Linear Ridge Regression benchmark.
+This class implements the Linear Ridge Regression benchmark for regression.
+
+Notes
+-----
+The following configurable options are available for this benchmark:
+* tau: The regularization constant.
+* bias: The bias in the linear hypothesis expression.
+* solver: "auto", "cplex", "glpk", "newton", "direct", "elasticnet",
+"block_norm"
 '''
 class SHOGUN_LINEARRIDGEREGRESSION(object):
+  '''
+  Create the Linear Ridge Regression benchmark instance.
+  
+  @type method_param - dict
+  @param method_param - Extra options for the benchmarking method.
+  @type run_param - dict
+  @param run_param - Path option for executing the benckmark. Not used for 
+  Shogun.
+  '''
   def __init__(self, method_param, run_param):
     self.info = "SHOGUN_LINEARRIDGEREGRESSION ("  + str(method_param) +  ")"
 
@@ -35,27 +62,74 @@ class SHOGUN_LINEARRIDGEREGRESSION(object):
     if len(self.data) >= 2:
       self.test_feat = RealFeatures(self.data[1].T)
 
-    if "alpha" in method_param:
-      self.tau = float(method_param["alpha"])
+    self.tau = 2
+    if "tau" in method_param:
+      self.tau = float(method_param["tau"])
 
+    self.bias = 0
+    if "bias" in method_param:
+      self.bias = float(method_param["bias"])
+
+    self.solver = "auto"
+    if "solver" in method_param:
+      self.solver = str(method_param["solver"])
+
+  '''
+  Return information about the benchmarking instance.
+
+  @rtype - str
+  @returns - Information as a single string.
+  '''
   def __str__(self):
     return self.info
 
+  '''
+  Calculate metrics to be used for benchmarking.
+
+  @rtype - dict
+  @returns - Evaluated metrics.
+  '''
   def metric(self):
     totalTimer = Timer()
+
     with totalTimer:
       model = LRR(self.tau, self.train_feat, self.train_labels)
+      model.set_bias(self.bias)
+
+      if self.solver == "auto":
+        model.set_solver_type(ST_AUTO)
+
+      elif self.solver == "cplex":
+        model.set_solver_type(ST_CPLEX)
+
+      elif self.solver == "glpk":
+        model.set_solver_type(ST_GLPK)
+
+      elif self.solver == "newton":
+        model.set_solver_type(ST_NEWTON)
+
+      elif self.solver == "direct":
+        model.set_solver_type(ST_DIRECT)
+
+      elif self.solver == "elasticnet":
+        model.set_solver_type(ST_ELASTICNET)
+
+      elif self.solver == "block_norm":
+        model.set_solver_type(ST_BLOCK_NORM)
+
+      else:
+        raise ValueError("Provided solver not supported by current benchmark")
+
       model.train()
 
       if len(self.data) >= 2:
-        model.apply_regression(self.test_feat)
+        predictions = model.apply(self.test_feat).get_labels()
 
 
     metric = {}
     metric["runtime"] = totalTimer.ElapsedTime()
 
     if len(self.data) == 3:
-      predictions = model.apply_regression(self.test_feat).get_labels()
       metric['MSE'] = Metrics.SimpleMeanSquaredError(self.data[2], predictions)
 
     return metric
